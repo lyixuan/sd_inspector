e2d42ef3bba35f19f740f62ff6992e742a3232f0
lyx <123>
2019-02-27 18:59:06 +0800

    merge

--- src/pages/m1/details/index.js
+++ src/pages/m1/details/index.js
@@ -1,18 +1,9 @@
-import Link from 'umi/link';
+import RadioComponent from '../../../components/Tabs'
+
 export default function() {
   return (
-    <div style={{border:"1px solid yellow",padding: '10px'}}>
-      我是detail下的页面
-      我是detail下的页面
-      我是detail下的页面
-      我是detail下的页面
-      我是detail下的页面
-      我是detail下的页面
-      我是detail下的页面
-      <br/>
-      <Link to="/m1/survey"> 概览 </Link>
-      <br/>
-      <Link to="/doc"> 文档 </Link>
+    <div  style={{border:"1px solid blue",padding: '10px'}}>
+      <RadioComponent path='/m1/details' />
     </div>
   );
 }
