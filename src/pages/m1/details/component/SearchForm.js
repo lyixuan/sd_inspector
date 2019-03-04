ca3937171744ad240bbe8f3e28be3d7ad58edb71
lyx <123>
2019-03-04 18:35:56 +0800

    详情 交互功能

--- src/pages/m1/details/component/SearchForm.js
+++ src/pages/m1/details/component/SearchForm.js
@@ -40,7 +40,7 @@ class HorizontalLoginForm extends React.Component {
       { id: 1, name: '我的条件1' },
       { id: 22, name: '我的条件22' }
     ];
-
+    this.checkedConditionList = {};
   }
   UNSAFE_componentWillMount() {
     this.props.dispatch({
@@ -68,20 +68,24 @@ class HorizontalLoginForm extends React.Component {
     })
   };
 
-  handleProChange = (v) => {
-    console.log(v);
-    if (v[v.length-1] === '000000') {
+  formValChange = (val, key) => {
+    let labels = undefined;
+    if (val instanceof Array) {
+      // 处理多选类型
       const list = [];
-      this.provinceAllList.forEach((v) => {
-        if (v.code !== '000000') {
-          list.push(v.code);
-        }
+      val.forEach((v) => {
+        list.push(v.label);
       });
-      console.log(list);
-      this.props.form.setFieldsValue({
-        provinceList: list,
-      });
+      labels = list.join(',');
+    } else {
+      labels = val.label;
     }
+    this.checkedConditionList[key] = labels;
+  };
+
+  handleReset = () => {
+    this.checkedConditionList= {};
+    this.props.form.resetFields();
   };
 
   handleSubmit = (e) => {
@@ -101,6 +105,8 @@ class HorizontalLoginForm extends React.Component {
     this.examList = this.props.global.examList;
     // this.conditionList = this.props.dataDetail.queryConditionList;
 
+    const checkedConditionList = this.checkedConditionList;
+
     const { getFieldDecorator, } = this.props.form;
     const menu = (
       <Menu>
@@ -111,143 +117,162 @@ class HorizontalLoginForm extends React.Component {
         ))}
       </Menu>
     );
+    console.log(checkedConditionList);
+
+    function getCheckedConditionList() {
+      const list = [];
+      for (let key in checkedConditionList) {
+        checkedConditionList[key] && list.push(checkedConditionList[key]);
+      }
+      return list.map((v) => (
+        <span className={styles.spanBtn} key={v}>{v}</span>
+      ))
+    }
 
     return (
       <Form layout="inline" onSubmit={this.handleSubmit}>
-        <span className={styles.rowTitle}>查询条件：</span>
-        <div className={styles.row}>
-          {/* 第一行 */}
-          <div>
-            <Form.Item label="考期">
-              {getFieldDecorator('exam', {
-                initialValue: this.state.exam,
-              })(
-                <Select placeholder="考期">
-                  {this.examList.map(item => (
-                    <Option key={item.id}>
-                      {item.exam}
-                    </Option>
-                  ))}
-                </Select>
-              )}
-            </Form.Item>
-          </div>
-          {/* 第二行 */}
-          <div>
-            <Form.Item label="学员信息">
-              {getFieldDecorator('provinceList', {
-                initialValue: this.state.provinceList,
-              })(
-                <Select placeholder="报考省份"  mode="multiple" showArrow={true} maxTagCount={1} onChange={this.handleProChange}>
-                  {this.provinceAllList.map(item => (
-                    <Option key={item.code}>
-                      {item.name}
-                    </Option>
-                  ))}
-                </Select>
-              )}
-            </Form.Item>
-            <Form.Item>
-              {getFieldDecorator('collegeId', {
-                initialValue: this.state.collegeId,
-              })(
-                <Select placeholder="学院">
-                  {this.collegeList.map(item => (
-                    <Option value={item.examId} key={item.examName}>
-                      {item.examName}
-                    </Option>
-                  ))}
-                </Select>
-              )}
-            </Form.Item>
-            <Form.Item>
-              {getFieldDecorator('familyIdList', {
-                initialValue: this.state.familyIdList,
-              })(
-                <Select placeholder="家族">
-                  {this.familyList.map(item => (
-                    <Option value={item.examId} key={item.examName}>
-                      {item.examName}
-                    </Option>
-                  ))}
-                </Select>
-              )}
-            </Form.Item>
-            <Form.Item>
-              {getFieldDecorator('orderStatus', {
-                initialValue: this.state.orderStatus,
-              })(
-                <Select placeholder="订单状态">
-                  {BiFilter('ORDER_STATE').map(item => (
-                    <Option value={item.id} key={item.name}>
-                      {item.name}
-                    </Option>
-                  ))}
-                </Select>
-              )}
-            </Form.Item>
-            <Form.Item>
-              {getFieldDecorator('stuType', {
-                initialValue: this.state.stuType,
-              })(
-                <Select placeholder="学员身份">
-                  {BiFilter('STUDENT_TYPE').map(item => (
-                    <Option value={item.id} key={item.name}>
-                      {item.name}
-                    </Option>
-                  ))}
-                </Select>
-              )}
-            </Form.Item>
-          </div>
-          {/* 第三行 */}
-          <div>
-            <Form.Item label="业务信息">
-              {getFieldDecorator('admissionStatus', {
-                initialValue: this.state.admissionStatus,
-              })(
-                <Select placeholder="准考证填写状态">
-                  {BiFilter('TICKET_STATES').map(item => (
-                    <Option value={item.id} key={item.name}>
-                      {item.name}
-                    </Option>
-                  ))}
-                </Select>
-              )}
-            </Form.Item>
-            <Form.Item>
-              {getFieldDecorator('msgStatusList', {
-                initialValue: this.state.msgStatusList,
-              })(
-                <Select placeholder="消息打开状态"  mode="multiple" showArrow={true} maxTagCount={1}>
-                  {BiFilter('MSG_STATES').map(item => (
-                    <Option key={item.id}>{item.name} </Option>
+        <div className={styles.searchBoxBg}>
+          <span className={styles.rowTitle}>查询条件：</span>
+          <div className={styles.row}>
+            {/* 第一行 */}
+            <div>
+              <Form.Item label="考期">
+                {getFieldDecorator('exam', {
+                  initialValue: this.state.exam,
+                })(
+                  <Select placeholder="考期" labelInValue onChange={(val) => this.formValChange(val,'exam')}>
+                    {this.examList.map(item => (
+                      <Option key={item.id}>
+                        {item.exam}
+                      </Option>
                     ))}
-                </Select>
-              )}
-            </Form.Item>
-          </div>
-          {/* 第四行 */}
-          <div style={{ marginTop: '60px' }}>
-            <Form.Item label="&nbsp;">
-              <Dropdown overlay={menu}>
-                <Button>
-                  {this.state.menuCheckedName} <Icon type="down" />
-                </Button>
-              </Dropdown>
-            </Form.Item>
-            <Form.Item style={{ marginLeft: '300px' }}>
-              <Button type="primary2">恢复默认</Button>
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit"><Icon type="search" />查询</Button>
-            </Form.Item>
+                  </Select>
+                )}
+              </Form.Item>
+            </div>
+            {/* 第二行 */}
+            <div>
+              <Form.Item label="学员信息">
+                {getFieldDecorator('provinceList', {
+                  initialValue: this.state.provinceList,
+                })(
+                  <Select placeholder="报考省份"  mode="multiple" showArrow={true} maxTagCount={1} labelInValue onChange={(val) => this.formValChange(val,'provinceList')}>
+                    {this.provinceAllList.map(item => (
+                      <Option key={item.code}>
+                        {item.name}
+                      </Option>
+                    ))}
+                  </Select>
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('collegeId', {
+                  initialValue: this.state.collegeId,
+                })(
+                  <Select placeholder="学院" labelInValue onChange={(val) => this.formValChange(val,'collegeId')}>
+                    {this.collegeList.map(item => (
+                      <Option value={item.examId} key={item.examName}>
+                        {item.examName}
+                      </Option>
+                    ))}
+                  </Select>
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('familyIdList', {
+                  initialValue: this.state.familyIdList,
+                })(
+                  <Select placeholder="家族" labelInValue onChange={(val) => this.formValChange(val,'familyIdList')}>
+                    {this.familyList.map(item => (
+                      <Option value={item.examId} key={item.examName}>
+                        {item.examName}
+                      </Option>
+                    ))}
+                  </Select>
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('orderStatus', {
+                  initialValue: this.state.orderStatus,
+                })(
+                  <Select placeholder="订单状态" labelInValue onChange={(val) => this.formValChange(val,'orderStatus')}>
+                    {BiFilter('ORDER_STATE').map(item => (
+                      <Option value={item.id} key={item.name}>
+                        {item.name}
+                      </Option>
+                    ))}
+                  </Select>
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('stuType', {
+                  initialValue: this.state.stuType,
+                })(
+                  <Select placeholder="学员身份" labelInValue onChange={(val) => this.formValChange(val,'stuType')}>
+                    {BiFilter('STUDENT_TYPE').map(item => (
+                      <Option value={item.id} key={item.name}>
+                        {item.name}
+                      </Option>
+                    ))}
+                  </Select>
+                )}
+              </Form.Item>
+            </div>
+            {/* 第三行 */}
+            <div>
+              <Form.Item label="业务信息">
+                {getFieldDecorator('admissionStatus', {
+                  initialValue: this.state.admissionStatus,
+                })(
+                  <Select placeholder="准考证填写状态" labelInValue onChange={(val) => this.formValChange(val,'admissionStatus')}>
+                    {BiFilter('TICKET_STATES').map(item => (
+                      <Option value={item.id} key={item.name}>
+                        {item.name}
+                      </Option>
+                    ))}
+                  </Select>
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('msgStatusList', {
+                  initialValue: this.state.msgStatusList,
+                })(
+                  <Select placeholder="消息打开状态"  mode="multiple" showArrow={true} maxTagCount={1} labelInValue onChange={(val) => this.formValChange(val,'msgStatusList')}>
+                    {BiFilter('MSG_STATES').map(item => (
+                      <Option key={item.id}>{item.name}</Option>
+                    ))}
+                  </Select>
+                )}
+              </Form.Item>
+            </div>
+            {/* 第四行 */}
+            <div style={{ marginTop: '60px' }}>
+              <Form.Item label="&nbsp;">
+                <Dropdown overlay={menu}>
+                  <Button>
+                    {this.state.menuCheckedName} <Icon type="down" />
+                  </Button>
+                </Dropdown>
+              </Form.Item>
+              <Form.Item style={{ marginLeft: '300px' }}>
+                <Button type="primary2" onClick={this.handleReset}>恢复默认</Button>
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit"><Icon type="search" />查询</Button>
+              </Form.Item>
+            </div>
           </div>
         </div>
-        <span className={styles.rowTitle}>已选条件：</span>
-        <div className={styles.row} style={{ background: 'transparent' }}>
-          <span className={styles.spanBtn}>条件我</span> <Button type="primary">保存查询条件</Button>
-        </div>
+        {
+          getCheckedConditionList().length > 0 ? (
+            <div>
+              <span className={styles.rowTitle}>已选条件：</span>
+              <div className={styles.row} style={{ background: 'transparent' }}>
+                {getCheckedConditionList()} <Button type="primary" style={{marginLeft:'20px'}}>保存查询条件</Button>
+              </div>
+            </div>
+          ):null
+        }
       </Form>
     );
   }
