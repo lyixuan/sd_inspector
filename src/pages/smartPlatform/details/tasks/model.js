73b98c9dc8ec0e55e7d063a6a06beb446262a13e
lixioajing <lixiaojing01@sunlands.com>
2019-03-05 22:01:47 +0800

    任务列表

--- src/pages/smartPlatform/details/tasks/model.js
+++ src/pages/smartPlatform/details/tasks/model.js
@@ -1,13 +1,48 @@
+import { message } from 'antd/lib/index';
+import { getTaskPage,deleteTask,reloadTask } from './service';
+
 export default {
   namespace: 'detail',
 
   state: {
+    tableList:[]
   },
 
   effects: {
+    // 任务列表
+    *getTaskPage({ payload }, { call, put }) {
+      const result = yield call(getTaskPage, payload.params);
+      const tableList = result.data || [];
+      if (result && result.code === 20000) {
+        yield put({ type: 'save', payload: { tableList } });
+      } else {
+        message.error(result.msg);
+      }
+    },
+    // 删除查询条件
+    *deleteTask({ payload }, { call, put }) {
+      const result = yield call(deleteTask, payload.params);
+      if (result.code === 20000) {
+        yield put({ type: 'save', payload: {} });
+      } else {
+        message.error(result.msg);
+      }
+    },
+    // 重新加载任务
+    *reloadTask({ payload }, { call, put }) {
+      const result = yield call(reloadTask, payload.params);
+      if (result.code === 20000) {
+        yield put({ type: 'save', payload: {} });
+      } else {
+        message.error(result.msg);
+      }
+    }
   },
 
   reducers: {
+    save(state, action) {
+      return { ...state, ...action.payload };
+    },
   },
 
   subscriptions: {
--- src/pages/smartPlatform/details/tasks/service.js
+++ src/pages/smartPlatform/details/tasks/service.js
@@ -1,3 +1,16 @@
+import request from '@/utils/request';
 
-export function getText() {
+// 任务列表
+export async function getTaskPage() {
+  return request('/detail/getTaskPage');
+}
+
+// 删除任务
+export async function deleteTask(params) {
+  return request('/detail/deleteTask', {method: 'delete',params});
+}
+
+// 删除任务
+export async function reloadTask(params) {
+  return request('/detail/reloadTask', {method: 'put',params});
 }
