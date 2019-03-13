4afddcb00c1c76f293a588dd07efc8a0950f4351
lixioajing <lixiaojing01@sunlands.com>
2019-03-13 11:05:21 +0800

    省份和家族图表组件

--- src/pages/smartPlatform/survey/component/EchartCommonOptions/family_prov_options.js
+++ src/pages/smartPlatform/survey/component/EchartCommonOptions/family_prov_options.js
@@ -12,13 +12,13 @@ export function famProOPtion(){
     legend: {
       bottom: 0,
       textStyle: {
-        fontSize: 12
+        fontSize: '12px'
       },
       itemWidth: 10,
       itemHeight: 10,
-      data: ['老生触达率', '新生触达率', '人均服务老生人数', '人均服务新生人数']
+      data: ['老生触达率', '新生触达率','人均服务老生人数', '人均服务新生人数']
     },
-    color:['#0080FF','#FF4165','#52C9C2','#FD9E3B'],
+    color:['#0080FF','#FF4165','#fff','#52C9C2','#FD9E3B'],
     grid: {
       left: '3%',
       right: '4%',
@@ -27,12 +27,22 @@ export function famProOPtion(){
     },
     xAxis: [{
       type: 'value',
+      axisTick: {
+        show: false,
+      },
+      show:false,
     },{
       type: 'value',
-    },],
+      show:false,
+    }],
     yAxis: {
       type: 'category',
-      data: ['巴西','印尼','美国','印度','中国','北京']
+      axisTick: {
+        show: false,
+      },
+      // inverse:true,
+      show:false,
+      // data: ['巴西','印尼','美国','印度','中国','北京']
     },
     series: [{
       name: '老生触达率',
@@ -48,17 +58,47 @@ export function famProOPtion(){
       symbol: 'circle',
       symbolSize: 6,
       data: [19325, 23438, 31000, 121594, 134141, 681807]
-    },
-      {
-        name: '人均服务老生人数',
-        type: 'bar',
-        data: [18203, 23489, 29034, 104970, 131744, 630230]
+    }, {
+      type: 'bar',
+      barWidth: 10,
+      label: {
+        show: true,
+        formatter:function (params) {
+          return params.name
+        },
+        position:  [0, 0],
+        color:'#000',
+        fontSize:'14px'
       },
-      {
-        name: '人均服务新生人数',
-        type: 'bar',
-        data: [19325, 23438, 31000, 121594, 134141, 681807]
-      }
+      data:  [{name:'巴西',value:134141},
+        {name:'印尼',value:134141},
+        {name:'美国',value:134141},
+        {name:'印度',value:134141},
+        {name:'中国',value:134141},
+        {name:'北京',value:134141}]
+    }, {
+      name: '人均服务老生人数',
+      type: 'bar',
+      barWidth: 10,
+      label: {
+        show: true,
+        position: 'right',
+        color:'#000',
+        fontSize:'12px',
+      },
+      data: [18203, 23489, 29034, 104970, 131744, 630230]
+    }, {
+      name: '人均服务新生人数',
+      type: 'bar',
+      barWidth: 10,
+      label: {
+        show: true,
+        position: 'right',
+        color:'#000',
+        fontSize:'12px',
+      },
+      data: [19325, 23438, 31000, 121594, 134141, 681807]
+    }
     ]
   };
 }
