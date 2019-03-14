1dd27560710354c3ba40402a51f243e87b07ad61
zhangyuwu <zhangyuwu@sunlands.com>
2019-03-14 15:37:17 +0800

    修改质检详情页

--- src/pages/qualityAppeal/components/subOrderDetail/index.js
+++ src/pages/qualityAppeal/components/subOrderDetail/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import styles from './style.css';
 
 export default class SubOrderDetailComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.data,
+    };
+  }
   componentDidMount() {}
 
   render() {
-    return <div>子订单详情</div>;
+    return (
+      <section>
+        <h1>子订单详情</h1>
+        <div className={styles.secRow}>
+          <div>学员姓名：{this.state.data.name}</div>
+          <div>报名时间：{this.state.data.time}</div>
+          <div>学员id：{this.state.data.id}</div>
+        </div>
+        <div className={styles.secRow}>
+          <div>联系方式：{this.state.data.contact}</div>
+          <div>产品包：{this.state.data.proPack}</div>
+          <div>缴费金额：{this.state.data.account}</div>
+        </div>
+        <div className={styles.secRow}>
+          <div>班主任：{this.state.data.department}</div>
+          <div>组织：{this.state.data.org}</div>
+        </div>
+      </section>
+    );
   }
 }
--- src/pages/qualityAppeal/qualityNewSheet/detail/index.js
+++ src/pages/qualityAppeal/qualityNewSheet/detail/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import styles from './style.less';
 import { connect } from 'dva';
 import SubOrderDetail from './../../components/subOrderDetail';
 
 class QualityDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subOrderDetail: {
+        name: '张三',
+        time: '2019年02月01日 21：22：30',
+        id: '00001',
+        contact: '18600540558',
+        proPack: '不过退费',
+        account: '4999元',
+        department: '李四',
+        org: '芝士学员|能源管理',
+      },
+    };
+  }
   render() {
     return (
-      <div>
-        <SubOrderDetail />
+      <div className={styles.detailContainer}>
+        <header />
+        <section>
+          <div>子订单编号：1234678</div>
+          <SubOrderDetail data={this.state.subOrderDetail} />
+        </section>
       </div>
     );
   }
