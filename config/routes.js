4ea72c7f7b82d105809723a9d6d42de966c04d3a
mzh <manzhenhua@sunlands.com>
2019-03-14 14:15:42 +0800

    删除文件

--- config/routes.js
+++ config/routes.js
@@ -29,7 +29,7 @@ export const routes = [
           {
             path: '/smartPlatform/exam',
             component: './smartPlatform/exam',
-            name:'报考',
+            name: '报考',
           },
           {
             path: '/smartPlatform/details/tasks',
@@ -54,7 +54,7 @@ export const routes = [
           },
           {
             path: '/qualityAppeal/qualityNewSheet/Create',
-            component: './qualityAppeal/qualityNewSheet/Create',
+            component: './qualityAppeal/qualityNewSheet/create',
             name: '创建质检单',
             bread: {
               name: '新质检单管理',
