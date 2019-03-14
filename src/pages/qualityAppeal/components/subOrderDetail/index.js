5f6f14781db0c7c46aa1a959349e0d580b0ee34f
zhangyuwu <zhangyuwu@sunlands.com>
2019-03-14 10:39:19 +0800

    增加detail

---
+++ src/pages/qualityAppeal/components/subOrderDetail/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class SubOrderDetailComponent extends React.Component {
+  componentDidMount() {}
+
+  render() {
+    return <div>子订单详情</div>;
+  }
+}
--- src/pages/qualityAppeal/qualityNewSheet/detail/index.js
+++ src/pages/qualityAppeal/qualityNewSheet/detail/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'dva';
+import { SubOrderDetail } from '@/pages/QualityApeal/components';
 
 class QualityDetail extends React.Component {
   render() {
