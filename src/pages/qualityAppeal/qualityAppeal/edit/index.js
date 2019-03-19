c7e4eef8734846550b85b6291672bbfe6b3e9b0c
zhangshuli <zhangshuli8041@163.com>
2019-03-19 09:57:28 +0800

    申诉编辑

--- src/pages/qualityAppeal/qualityAppeal/edit/index.js
+++ src/pages/qualityAppeal/qualityAppeal/edit/index.js
@@ -2,14 +2,81 @@ import React from 'react';
 import { connect } from 'dva';
 import styles from './style.less';
 import { Row, Col, Input, Radio } from 'antd';
+import BIButton from '@/ant_components/BIButton';
+import PersonInfo from './../../qualityNewSheet/detail/components/personInfo';
+import AppealInfo from './../component/appealInfo';
+import SuperiorCheck from './../component/superiorCheck';
 const { TextArea } = Input;
 const RadioGroup = Radio.Group;
 
 
 class EditAppeal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      qualityData: {
+        verifyDate: '2019年02月01日 21：22：30',
+        mail: 'test@sunlands.com',
+        role: '班主任',
+        name: '李思思',
+        collegeName: null,
+        familyName: null,
+        orderNum: 123456789,
+        groupName: '芝士学院|能源管理|运营1组',
+        violationDate: '2019年02月01日 21：22：30',
+        reduceScoreDate: '2019年02月01日 21：22：30',
+        qualityType: '班主任质检',
+        dimension: '超高危',
+        primaryAssortment: '服务禁语规范',
+        secondAssortment: '禁止沟通中消极对待用户',
+        thirdAssortment: '冷漠、不热情、不耐烦',
+        violationLevel: '一级违规（扣除学分1000分）',
+        attUrl: '附件1',
+        desc:
+          '违规描述违规描述违规描述违规描述违规描述违规描述违规描述违规描述违规描述违规描述违规描述违规描述',
+        orderDetail: {
+          stuName: '张三',
+          signDate: '2019年02月01日 21：22：30',
+          stuId: '00001',
+          phoneNum: '18600540558',
+          produce: '不过退费',
+          payment: '4999元',
+          teaName: '李四',
+          groupName: '芝士学员|能源管理',
+        },
+        verifyDate: '2019年02月01日 21：22：30',
+        appealEndDate: '2019年02月01日 21：22：30',
+        operateDate: '2019年02月01日 21：22：30',
+        desc: '没有违规',
+        operator: '张三',
+        sopCheckDetail: [
+          {
+            sopCheckResult: '审核通过',
+            verifyDate: '2019年02月01日 21：22：30',
+            checkDesc: '审核通过',
+            operator: '张三',
+            sign: true,
+          },
+          {
+            sopCheckResult: '驳回',
+            verifyDate: '2019年02月01日 21：22：30',
+            checkDesc: '审核通过',
+            operator: '张三',
+            sign: false,
+          },
+        ],
+      },
+    };
+  }
   render() {
     return (
       <div className={styles.editAppeal}>
+        <section>
+          <PersonInfo data={this.state.qualityData} />
+        </section>
+        <section>
+          <AppealInfo data={this.state.qualityData} />
+        </section>
         <div className={styles.editBox}>
           <div className={styles.title}>SOP审核</div>
           <Row className="gutter-row2">
@@ -32,6 +99,23 @@ class EditAppeal extends React.Component {
             </Col>
           </Row>
         </div>
+
+        <section>
+          <SuperiorCheck data={this.state.qualityData.sopCheckDetail} />
+        </section>
+
+        <Row className="gutter-row">
+          <Col span={24}>
+            <div className={styles.gutterBox1}>
+              <span className={styles.gutterBtn2}>
+                <BIButton>取消</BIButton>
+              </span>
+              <span className={styles.gutterBtn1}>
+                <BIButton type="primary" htmlType="submit">提交</BIButton>
+              </span>
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
