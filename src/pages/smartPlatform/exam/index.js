485a4841876bd47fe94fa0542c98c10eaa21e3f3
lixioajing <lixiaojing01@sunlands.com>
2019-03-19 11:05:34 +0800

    样式修改

--- src/pages/smartPlatform/exam/index.js
+++ src/pages/smartPlatform/exam/index.js
@@ -167,7 +167,7 @@ class Survey extends React.Component {
                   <div className='m_box'><Echart isEmpty={colDataList[tabId]?colDataList[tabId].data1.length === 0:false} update={`${JSON.stringify(colDataList)}${tabId}`} style={{ width: '100%', height: "410px" }} options={blendChartOptions(this.state,colDataList,'all',undefined,unit,tabId)} /></div>
                   <div className='m_box'><Echart isEmpty={famDataMap[tabId]?famDataMap[tabId].data1.length === 0:false} update={`${JSON.stringify(famDataMap)}${tabId}`} style={{ width: '100%', height:`${famDataMap[tabId]?famDataMap[tabId].data1.length*50:1700}px` }} options={famProOPtion(this.state,famDataMap,'fam',undefined,unit,tabId)} /></div>
                   <div className='m_box'>
-                    <Echart isEmpty={groDataList[tabId]?groDataList[tabId].data1.length === 0:false} update={`${JSON.stringify(groDataList)}${isShowMore}`} style={{width:'100%',height:isShowMore?`${groDataList[tabId].data1.length*48}px`:'960px'}} options={groupOPtion(this.state,groDataList,unit)} />
+                    <Echart isEmpty={groDataList[tabId]?groDataList[tabId].data1.length === 0:false} update={`${JSON.stringify(groDataList)}${isShowMore}`} style={{width:'100%',height:isShowMore?`${groDataList[tabId].data1.length*48}px`:'1100px'}} options={groupOPtion(this.state,groDataList,unit)} />
                     <BIButton type="primary" style={{marginBottom:'20px',display:isShowMore?'none':'block'}} onClick={this.getMoreData}>查看更多</BIButton>
                   </div>
                 </div>
