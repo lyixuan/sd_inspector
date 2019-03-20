21638ecb01fc0648cd827dac7b0e14a64457331f
lyx <123>
2019-03-20 11:49:44 +0800

    质检

--- src/pages/qualityAppeal/qualityNewSheet/services.js
+++ src/pages/qualityAppeal/qualityNewSheet/services.js
@@ -2,6 +2,6 @@ import request from '@/utils/request';
 
 // 查询质检列表
 export async function getQualityList(params) {
-  return request('/quality/quality/queryQualityDataPage', {method: 'post',data: params});
+  return request('/quality/queryQualityDataPage', {method: 'post',data: params});
 }
 
--- src/pages/qualityAppeal/services.js
+++ src/pages/qualityAppeal/services.js
@@ -3,12 +3,12 @@ import request from '@/utils/request';
 
 // 查询组织架构
 export async function getOrgMapList(params) {
-  return request('/quality/orgMap/getOrgMapList', { params });
+  return request('/orgMap/getOrgMapList', { params });
 }
 
 // 查询分维
 export async function getDimensionList(params) {
-  return request('/quality/dimension/rootList', { params });
+  return request('/dimension/rootList', { params });
 }
 
 
