1bb24b4809e7adb09bff3a40907b225e97a41b21
zhangyuwu <zhangyuwu@sunlands.com>
2019-03-25 14:13:55 +0800

    修改质检详情页样式

--- src/pages/qualityAppeal/qualityAppeal/component/appealInfo/index.js
+++ src/pages/qualityAppeal/qualityAppeal/component/appealInfo/index.js
@@ -7,62 +7,62 @@ export default class AppealInfoComponent extends React.Component {
     super(props);
     this.state = {
       data: props.data,
-      appealInfoCollapse: props.appealInfoCollapse,
+      appealInfoCollapse: false,
     };
   }
   componentDidMount() {}
-  appealPanelVisible() {
-    this.props.onClick();
+  appealPanelVisible(index) {
+    this.props.onClick(index);
   }
   getAppealStatus() {
     if (this.state.appealInfoCollapse) {
-      return '-';
+      return '+';
     }
-    return '+';
+    return '-';
   }
   render() {
-    const { appealEndDate, appealStart, type } = this.props.data;
-    const appealInfoCollapse = this.props.appealInfoCollapse;
+    const { appealEndDate, appealStart, type, index, isCollapse } = this.props.data;
+    this.state.appealInfoCollapse = isCollapse;
+    console.log(isCollapse);
     return (
       <section className={styles.personInfoCon}>
-        <span className={styles.secctionTitle}>申诉信息</span>
+        <span className={[index == 0 ? styles.secctionTitle : `${styles.hidePanel}`]}>
+          申诉信息
+        </span>
         <div className={styles.appealInfoCon}>
           <div className={styles.appealInfo}>
             {type}次申诉
             <span>一次申诉截止日期：{moment(appealEndDate).format('YYYY-MM-DD HH:mm:ss')}</span>
             <span
               onClick={() => {
-                this.appealPanelVisible();
+                this.appealPanelVisible(index);
               }}
               className={styles.appealInfoPanel}
             >
               {this.getAppealStatus()}
             </span>
           </div>
-          <article
-            className={[
-              styles.appealPerson,
-              appealInfoCollapse ? `${styles.showPanel} ` : `${styles.hidePanel}`,
-            ]}
-          >
-            <div className={styles.secctionTitle}>申诉发起人</div>
-            <div className={styles.container}>
-              <div className={styles.secRow}>
-                <div>附件：{appealStart.attUrl ? appealStart.attUrl : ''} </div>
-              </div>
-              <div className={[styles.secRow]}>
-                <div>
-                  <span>执行人：{appealStart.operator}</span>
-                  <span>
-                    操作时间：{moment(appealStart.operateDate).format('YYYY-MM-DD HH:mm:ss')}
-                  </span>
+          <article className={isCollapse ? `${styles.hidePanel}` : `${styles.showPanel} `}>
+            <div className={styles.appealPerson}>
+              <div className={styles.secctionTitle}>申诉发起人</div>
+              <div className={styles.container}>
+                <div className={styles.secRow}>
+                  <div>附件：{appealStart.attUrl ? appealStart.attUrl : ''} </div>
+                </div>
+                <div className={[styles.secRow]}>
+                  <div>
+                    <span>执行人：{appealStart.operator}</span>
+                    <span>
+                      操作时间：{moment(appealStart.operateDate).format('YYYY-MM-DD HH:mm:ss')}
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>
-              <div className={styles.secCol}>
-                <div>申诉说明：{appealStart.desc}</div>
-                <div />
+              <div>
+                <div className={styles.secCol}>
+                  <div>申诉说明：{appealStart.desc}</div>
+                  <div />
+                </div>
               </div>
             </div>
           </article>
