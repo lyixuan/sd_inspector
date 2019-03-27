964aa24731272f6b1d458db49eba660426cac36f
lixioajing <lixiaojing01@sunlands.com>
2019-03-27 18:03:57 +0800

    审核结果为空的时候不展示数据

--- src/pages/qualityAppeal/components/AppealInfo/qualityAppeal.js
+++ src/pages/qualityAppeal/components/AppealInfo/qualityAppeal.js
@@ -7,13 +7,18 @@ import styles from './style.less';
 class QualityAppeal extends React.Component {
   render() {
     const {setStateData,data} = this.props;
+    console.log(data)
     return (
       <div className={styles.appealWrap} style={{marginLeft:'-20px'}}>
         <div className={styles.mtitle}>质检审核</div>
-        <div className={styles.resultWrap}>
-          <div className={styles.s2_title}>审核记录</div>
-          <Info data={data}/>
-        </div>
+        {
+          data.length>0?(
+            <div className={styles.resultWrap}>
+              <div className={styles.s2_title}>审核记录</div>
+              <Info data={data}/>
+            </div>
+          ):null
+        }
         {setStateData?<Edit {...this.props} dataName='一审截止日期' showWarn={false} setStateData={setStateData}/>:null}
       </div>
     );
