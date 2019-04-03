3d86617d0b3554be783817fbc4ce72156512e787
zhangyuwu <zhangyuwu@sunlands.com>
2019-04-03 19:09:01 +0800

    删除service临时路径

--- src/pages/smartPlatform/survey/services.js
+++ src/pages/smartPlatform/survey/services.js
@@ -5,11 +5,10 @@ export async function queryHistogramData(params) {
   return request('/general/queryHistogramData', {
     method: 'post',
     data: params,
-    prefix: '/tmpApi',
   });
 }
 // 获取地图数据相关接口
 
 export async function getMapInfo() {
-  return request('/general/getMapInfo', { prefix: '/tmpApi' });
+  return request('/general/getMapInfo');
 }
