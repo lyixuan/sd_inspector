1ddd0c5c94e0ac7a04e9fd26d90110e7f4148637
manzhenhua <309340538@qq.com>
2019-04-04 17:57:14 +0800

    报考详情页面

--- config/webpack.js
+++ config/webpack.js
@@ -2,15 +2,16 @@
 // 根据API_ENV环境不同分为debugger开发代理(api),development为使用dev的api,production为使用线上api
 const proxyHost = {
     localhost: 'http://127.0.0.1:8096',
-    development: 'http://172.16.117.65:8096', //测试
+    // development: 'http://172.16.117.65:8096', //测试
+    development: 'http://api.bd.ministudy.com',
     production: 'http://api.bd.ministudy.com',
 };
 const proxy_env = proxyHost[process.env.PROXY_ENV];
 export const webpackConfig = {
 
     externals: {
-        echarts: 'echarts',
-        d3: 'd3',
+        // echarts: 'echarts',
+        // d3: 'd3',
     },
     define: {
         'process.env.LOGIN_TYPE': process.env.LOGIN_TYPE,
