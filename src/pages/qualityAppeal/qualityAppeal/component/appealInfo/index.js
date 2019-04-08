ae4f3f851936d2fcd8bab4050d283a75651b56a7
lixioajing <lixiaojing01@sunlands.com>
2019-04-08 19:56:45 +0800

    样式修改

--- src/pages/qualityAppeal/qualityAppeal/component/appealInfo/index.js
+++ src/pages/qualityAppeal/qualityAppeal/component/appealInfo/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 import styles from './style.css';
 import moment from 'moment';
 import DownLoad from '@/components/DownLoad';
@@ -21,27 +22,20 @@ export default class AppealInfoComponent extends React.Component {
     const number = Number(type)===2?'二':'一';
     const InfoList = appealStart.map((v)=>(
       <div>
-        <div className={styles.container}>
-          <div className={styles.secRow}>
-            <div>
-              <span style={{float:'left',marginLeft:0}}>附件：</span> {v.attUrl?<DownLoad loadUrl={`${STATIC_HOST}/${v.attUrl}`} text={v.attUrl&&v.attUrl.split('/')[3]} fileName={v.attUrl&&v.attUrl.split('/')[3]} textClassName={styles.downCls}/>:null}
-            </div>
-          </div>
-          <div className={[styles.secRow]}>
-            <div>
-              <span>执行人：{v.operator}</span>
-              <span>
-                      操作时间：{moment(v.operateDate).format('YYYY年MM月DD日 HH:mm:ss')}
-                    </span>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className={styles.secCol}>
-            <div>申诉说明：{v.desc}</div>
-            <div />
-          </div>
-        </div>
+        <Row className={styles.container}>
+          <Col span={12}>
+            <span style={{float:'left',marginLeft:0}}>附件：</span> {v.attUrl?<DownLoad loadUrl={`${STATIC_HOST}/${v.attUrl}`} text={v.attUrl&&v.attUrl.split('/')[3]} fileName={v.attUrl&&v.attUrl.split('/')[3]} textClassName={styles.downCls}/>:null}
+          </Col>
+          <Col span={4}>
+            <span>执行人：{v.operator}</span>
+          </Col>
+          <Col span={8}>
+            <span>操作时间：{moment(v.operateDate).format('YYYY年MM月DD日 HH:mm:ss')} </span>
+          </Col>
+        </Row>
+        <Row className={styles.container}>
+          <Col span={24} ><span>申诉说明：{v.desc}</span></Col>
+        </Row>
       </div>
     ));
     return (
