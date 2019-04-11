865e52151d641f740345b06e18e43b07fbb6c3cd
lixioajing <lixiaojing01@sunlands.com>
2019-04-11 14:25:18 +0800

    首页

--- src/pages/ko/index.js
+++ src/pages/ko/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 class koPlan extends React.Component {
   componentDidMount() {}
+  jumpTo=(pathname)=>{
+    this.props.history.push({
+      pathname,
+    });
+  }
   render() {
-    return <div>{this.props.children}</div>;
+    return(
+      <div>
+        <div onClick={()=>this.jumpTo('/ko/behavior')}>行为分析</div>
+        <div onClick={()=>this.jumpTo('/ko/userList')}>用户列表</div>
+      </div>
+    ) ;
   }
 }
 
