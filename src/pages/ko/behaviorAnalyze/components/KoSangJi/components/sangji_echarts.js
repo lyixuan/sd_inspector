f8f47a5c0640c4c7518f5c3a96de84398f62d9d4
lyx <123>
2019-04-12 15:43:51 +0800

    ko

--- src/pages/ko/behaviorAnalyze/components/KoSangJi/components/sangji_echarts.js
+++ src/pages/ko/behaviorAnalyze/components/KoSangJi/components/sangji_echarts.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import Echart from '@/components/Echart';
+import Echart from '@/components/Echart_SangJi';
+import {getSangJiUpOption} from './sangji_options_up';
+import {getSangJiDownOption} from './sangji_options_down';
 
 class SangJiEcharts extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      option: {},
+    };
+  }
+  componentDidMount() {
+    const {position = 'left'} =  this.props;
+    this.setState({
+      option: position === 'left'?getSangJiUpOption():getSangJiDownOption()
+    })
+  }
   render() {
     return (
       <div>
+        <Echart style={{width:'300px',height:'300px'}} options={this.state.option} />
       </div>
     );
   }
