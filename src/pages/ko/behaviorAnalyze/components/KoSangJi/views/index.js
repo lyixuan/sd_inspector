2e1b9701c794aea9431754f8ae9e5a8be4e69223
lixioajing <lixiaojing01@sunlands.com>
2019-04-12 18:11:14 +0800

    转化率组件

---
+++ src/pages/ko/behaviorAnalyze/components/KoSangJi/views/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './index.less'
+
+class Views extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      option: {},
+    };
+  }
+  componentDidMount() {
+
+  }
+  render() {
+    const dataObj = [{name:'PV',data:12121},{name:'UV',data:121},{name:'跳出率',data:'20%'},{name:'转化率',data:'5%'}]
+    return (
+      <div className={styles.contentWrap}>
+        {
+          dataObj.map((item)=>{
+            return (
+              <div className={styles.itemCls}>
+                <div className={styles.nameCls}>{item.name}</div>
+                <div className={styles.dataCls}>{item.data}</div>
+              </div>
+            )
+          })
+        }
+
+      </div>
+    );
+  }
+}
+
+export default Views;
