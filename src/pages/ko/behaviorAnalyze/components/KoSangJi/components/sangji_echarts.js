11e53ec98d668eaf62bced316e0294dc9b378a11
lyx <123>
2019-04-12 15:29:34 +0800

    ko

---
+++ src/pages/ko/behaviorAnalyze/components/KoSangJi/components/sangji_echarts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Echart from '@/components/Echart';
+
+class SangJiEcharts extends React.Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default SangJiEcharts;
--- src/pages/ko/behaviorAnalyze/components/KoSangJi/index.js
+++ src/pages/ko/behaviorAnalyze/components/KoSangJi/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SangJiEcharts from './components/sangji_echarts';
 import style from './style.less';
 
 class KoSangJi extends React.Component {
@@ -6,6 +7,8 @@ class KoSangJi extends React.Component {
   render() {
     return (
       <div className={style.KoSangJi}>
+        <SangJiEcharts position={'left'}></SangJiEcharts>
+        <SangJiEcharts position={'right'}></SangJiEcharts>
       </div>
     );
   }
