573a8a310877c06e13c077e8959d5a296c4a9cc4
lyx <123>
2019-04-15 14:11:50 +0800

    usertable

--- src/pages/ko/userList/index.js
+++ src/pages/ko/userList/index.js
@@ -1,21 +1,124 @@
 import React from 'react';
 import { connect } from 'dva';
+import BITable from '@/ant_components/BITable';
+import BIPagination from '@/ant_components/BIPagination';
+import { BiFilter } from '@/utils/utils';
+import style from './style.less';
+import moment from 'moment/moment';
 
-@connect(({ userList }) => ({
-  userList,
+const columns = [
+  {
+    title: '学员',
+    dataIndex: 'qualityNum',
+  },
+  {
+    title: '注册状态',
+    dataIndex: 'qualityType',
+    render: (text, record) => {
+      return (
+        <>
+          {BiFilter(`QUALITY_TYPE|id:${record.qualityType}`).name}
+        </>
+      );
+    },
+  },
+  {
+    title: '选课状态',
+    dataIndex: 'violationName',
+  },
+  {
+    title: '浏览页面数量',
+    dataIndex: 'operateName',
+  },
+  {
+    title: '视频播放次数',
+    dataIndex: 'violationLevel',
+  },
+  {
+    title: '做题次数',
+    dataIndex: 'violationLevel',
+  },
+  {
+    title: 'IM咨询次数',
+    dataIndex: 'violationLevel',
+  },
+  {
+    title: '排队次数',
+    dataIndex: 'violationLevel',
+  },
+  {
+    title: '留言次数',
+    dataIndex: 'violationLevel',
+  },
+  {
+    title: '跟帖次数',
+    dataIndex: 'violationLevel',
+  },
+  {
+    title: '微信咨询次数',
+    dataIndex: 'violationLevel',
+  },
+  {
+    title: 'KO转化',
+    dataIndex: 'status',
+    render: (text, record) => {
+      function dot() {
+        let rt = null;
+        if (record.status === 4) {
+          rt = <span className={style.dotStl} style={{ background: '#52C9C2' }}></span>
+        }
+        if (record.status === 3) {
+          rt = <span className={style.dotStl} style={{ background: '#D9D9D9' }}></span>
+        }
+        return rt;
+      }
+      return (
+        <>
+          {dot()}
+          {BiFilter(`QUALITY_STATE|id:${record.status}`).name}
+        </>
+      );
+    },
+  },
+];
+@connect(({ userListModel,loading }) => ({
+  userListModel,
+  loading: loading.effects['userListModel/userList'],
 }))
 class userList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      page: 1,
+      pageSize: 30
     };
   };
+  onPageChange = (currentPage)=>{
+    this.queryData({page:currentPage});
+  };
+  queryData = (page) => {
+    let params = { ...this.state };
+    if (page) {
+      params = { ...params, ...page };
+      this.setState({
+        page: page.page
+      });
+    }
+    this.props.dispatch({
+      type: 'userListModel/userList',
+      payload: { params },
+    });
+  };
   render() {
-
+    const {userList,page={},loading} = this.props.userListModel;
+    const dataSource = userList;
     return (
       <div>
-        ddd
+        <div className={style.contentWrap}>
+          <BITable rowKey={record=>record.id + Math.random()*1000} dataSource={dataSource} columns={columns} pagination={false} loading={loading} />
+          <br/>
+          <BIPagination showQuickJumper defaultPageSize={page.pageSize?page.pageSize:30} onChange={this.onPageChange} current={page.pageNum} total={page.total} />
+        </div>
       </div>
     );
   }
--- src/pages/ko/userList/model.js
+++ src/pages/ko/userList/model.js
@@ -1,12 +1,26 @@
 import { message } from 'antd';
+import { queryUserList } from '@/pages/ko/userList/services';
+import { msgF } from '@/utils/utils';
 
 export default {
-  namespace: 'userList',
+  namespace: 'userListModel',
 
-  state: { },
+  state: {
+    userList: [],
+  },
 
   effects: {
+    *getUserList({ payload }, { call, put }) {
+      const params = payload.params;
+      const result = yield call(queryUserList, params);
+      const userList = result.data || [];
 
+      if (result.code === 20000) {
+        yield put({ type: 'save', payload: { userList } });
+      } else {
+        message.error(msgF(result.msg,result.msgDetail));
+      }
+    },
   },
 
   reducers: {
