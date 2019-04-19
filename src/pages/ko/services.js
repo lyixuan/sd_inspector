255bf6540e3105680a2acb4bde4f51d84bf1e80f
lyx <123>
2019-04-19 11:47:22 +0800

    接口

--- src/pages/ko/services.js
+++ src/pages/ko/services.js
@@ -4,3 +4,24 @@ import request from '@/utils/request';
 export async function getKOEnumList(params) {
     return request('/homePage/enumList', { params });
 }
+
+// 页面下拉二级列表
+export async function getPageList(params) {
+  return request('/homePage/pageDetailInfoList', { params });
+}
+
+// 桑吉图结构接口
+export async function getSankeyMapOrg(params) {
+  return request('/homePage/sankeyMapOrg', { params });
+}
+
+// 桑吉图数据接口
+export async function getSankeyMapData(params) {
+  return request('/homePage/sankeyMapData', { params });
+}
+
+
+// table列表数据
+export async function getDataList(params) {
+  return request('/homePage/sankeyMapData', { params });
+}
