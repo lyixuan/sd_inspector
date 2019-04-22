a83e3b394c0bc5e0ab190aa94a07c2953bf80ecb
lixioajing <lixiaojing01@sunlands.com>
2019-04-22 20:02:36 +0800

    热力图ui修改

--- src/pages/ko/behaviorAnalyze/model.js
+++ src/pages/ko/behaviorAnalyze/model.js
@@ -24,7 +24,7 @@ export default {
     },
     saveDataList(state, { payload }) {
       const {hotDataList}=payload;
-      for(let i=0;i<10;i++){
+      for(let i=0;i<15;i++){
         hotDataList.push({
           name:`d${i+1}`,
           clickPeople:i*10,//点击人数
@@ -33,8 +33,19 @@ export default {
           countRate:(i/20*100).toFixed(2),//点击次数
         })
       }
-
       return { ...state, ...payload };
+      // const newHotData = [];
+      // hotDataList.forEach((item,i) => {
+      //   const {actionKey,clickNum,clickPeople} = item.actionEventData;
+      //   newHotData.push({
+      //     name:actionKey,
+      //     clickPeople:item.clickPeople,//点击人数
+      //     peopoleRate:(item.clickPeople/clickPeople*100).toFixed(2),//人数占比
+      //     clickCountPre:item.clickNum,//点击次数
+      //     countRate:(item.clickNum/clickNum*100).toFixed(2),//次数占比
+      //   })
+      // })
+      // return { ...state,hotDataList:newHotData };
     }
   },
 
