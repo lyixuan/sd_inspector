ab677f7cc48ceaff7a4b02892ce8836213b5ed9e
lixioajing <lixiaojing01@sunlands.com>
2019-05-14 20:07:25 +0800

    标题样式修改

--- src/pages/ko/behaviorAnalyze/components/KoSangJi/components/HotImg/KO_detail.js
+++ src/pages/ko/behaviorAnalyze/components/KoSangJi/components/HotImg/KO_detail.js
@@ -151,6 +151,7 @@ class KoDetailPage extends React.Component {
       this.chart.selectAll('.textWrap1 text').attr('dominant-baseline',"inherit").attr('text-anchor',"left");
       this.chart.selectAll('.textWrap11 text').attr('text-anchor',"start");
       this.chart.selectAll('.textWrap12 text').attr('text-anchor',"start");
+      this.chart.selectAll('.titleName').attr('text-anchor',"start");
 
       const colorArr = this.getColorFn(data);
 
