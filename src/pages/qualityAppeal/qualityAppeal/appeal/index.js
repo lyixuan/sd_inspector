2a641dc4d88e1ceb19caa435365e35649fac6d0a
lyx <123>
2019-05-27 10:37:17 +0800

    添加质检邮件需要的字段

--- src/pages/qualityAppeal/qualityAppeal/appeal/index.js
+++ src/pages/qualityAppeal/qualityAppeal/appeal/index.js
@@ -94,7 +94,9 @@ class QualityAppealing extends React.Component {
     const otherObj = {
       violationLevelName:BiFilter(`VIOLATION_LEVEL|id:${qualityDetailData.violationLevel}`).name,
       violationName:qualityDetailData.dimension,
-      firstAppealEndDate:this.query.firstAppealEndDate
+      firstAppealEndDate:this.query.firstAppealEndDate,
+      qualityValue:this.query.qualityValue,
+      qualityType:this.query.qualityType,
     }
     if (appealParam.checkResult !== 0 && !appealParam.checkResult) {
       message.warn('审核结果为必选项');
