c1fc2e4ac25a870bec4ef345bd8d7fc3b5724aac
lyx <123>
2019-06-05 18:02:33 +0800

    待申诉详情联调

--- src/pages/scoreAppeal/finishAppeal/index.js
+++ src/pages/scoreAppeal/finishAppeal/index.js
@@ -117,6 +117,9 @@ class FinishAppeal extends React.Component {
 
     if (dimensionType) {
       params = { ...params, ...{dimensionType} };
+      this.setState({
+        dimensionType
+      });
     }
 
     const saveUrlParams =JSON.stringify(params);
@@ -145,7 +148,8 @@ class FinishAppeal extends React.Component {
     });
   };
   onDetail = (record) => {
-    this.onJumpPage({ dimensionId: record.metaDimensionId,dimensionType:record.dimensionType }, '/scoreAppeal/appeal_detail');
+    const {dimensionType} = this.state;
+    this.onJumpPage({ dimensionId: record.metaDimensionId,dimensionType }, '/scoreAppeal/appeal_detail');
   };
   columnsAction = () => {
     const actionObj = [{
