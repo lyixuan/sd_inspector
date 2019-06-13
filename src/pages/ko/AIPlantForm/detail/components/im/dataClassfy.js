de2ef091f99ea6057f858836d975b4f820587091
zhangshuli <zhangshuli8041@163.com>
2019-06-13 10:56:51 +0800

    page

--- src/pages/ko/AIPlantForm/detail/components/im/dataClassfy.js
+++ src/pages/ko/AIPlantForm/detail/components/im/dataClassfy.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Icon } from 'antd';
 import { connect } from 'dva';
+import BIButton from '@/ant_components/BIButton';
+import BISelect from '@/ant_components/BISelect/formSelect';
 import styles from '../../style.less';
 
 class DataClassfy extends React.Component {
@@ -14,7 +16,22 @@ class DataClassfy extends React.Component {
   render() {
     return (
       <>
-        <div>fdfdf33</div>
+        <div className={styles.consultContent}>
+          <ul className={styles.consultInput}>
+            <li>
+              <labe>咨询类型</labe>
+              <div className={styles.selects}>
+
+              </div>
+            </li>
+          </ul>
+          <div className={styles.btn}>
+            <BIButton type="primary">
+              提交，下一条
+            </BIButton>
+          </div>
+          <div className={styles.progress}></div>
+        </div>
       </>
     );
   }
