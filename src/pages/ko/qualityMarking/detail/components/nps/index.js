95d039b3849d53057db9a4c9fb855e2eb28ca389
zhangshuli <zhangshuli8041@163.com>
2019-06-21 14:35:58 +0800

    detail

--- src/pages/ko/qualityMarking/detail/components/nps/index.js
+++ src/pages/ko/qualityMarking/detail/components/nps/index.js
@@ -4,6 +4,29 @@ import { Icon } from 'antd';
 import { connect } from 'dva';
 import styles from '../../style.less';
 
+function TagList(props) {
+  console.log(8, props)
+  if (props.list.length < 1) {
+    return null;
+  }
+  const li = props.list.map((item, index) =>
+    <li className={styles.flex}>
+      <div className={styles.row}>
+        <span className={styles.label}>{item.name}：</span>
+        <span className={styles.name + " " + styles.labels}>
+          <Label label={item.keywordList}></Label>
+        </span>
+      </div>
+    </li>
+  );
+  return li;
+}
+function Label(props) {
+  const label = props.label.map((item, index) =>
+    <em>{item}</em>
+  );
+  return label
+}
 class DetailNps extends React.Component {
   constructor(props) {
     super(props);
@@ -19,32 +42,35 @@ class DetailNps extends React.Component {
   }
 
   render() {
+    const { item } = this.props.pageData
+    console.log(23, item)
     return (
       <>
         <ul className={styles.userInfo}>
           <li className={styles.flex}>
             <div className={`${styles.row} ${styles.width50}`}>
               <span className={styles.label}>学员姓名：</span>
-              <span className={styles.name + " " + styles.nameCurrent}>玉青1龙</span>
+              <span className={styles.name + " " + styles.nameCurrent}>{item.stuName}</span>
             </div>
             <div className={`${styles.row} ${styles.width50}`}>
               <span className={styles.label}>学员id：</span>
-              <span className={styles.name}>388383</span>
+              <span className={styles.name}>{item.stuId}</span>
             </div>
           </li>
           <li className={styles.flex}>
             <div className={styles.row}>
               <span className={styles.label}>订&nbsp;&nbsp;单&nbsp;&nbsp;id：</span>
-              <span className={styles.name}>899889</span>
+              <span className={styles.name}>{item.ordId}</span>
             </div>
           </li>
           <li className={styles.flex}>
             <div className={styles.row}>
               <span className={styles.label}>星&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;级：</span>
-              <span className={styles.name}>4</span>
+              <span className={styles.name}>{item.starLevel}</span>
             </div>
           </li>
-          <li className={styles.flex}>
+          <TagList list={item.tagList}></TagList>
+          {/* <li className={styles.flex}>
             <div className={styles.row}>
               <span className={styles.label}>班主任服务：</span>
               <span className={styles.name + " " + styles.labels}>
@@ -53,27 +79,12 @@ class DetailNps extends React.Component {
                 <em>吐槽客服态度</em>
               </span>
             </div>
-          </li>
-          <li className={styles.flex}>
-            <div className={styles.row}>
-              <span className={styles.label}>授课服务：</span>
-              <span className={styles.name + " " + styles.labels}>
-                <em>吐槽客服态度吐槽客服态度</em>
-                <em>吐槽客服态度</em>
-                <em>吐槽客服态度</em>
-              </span>
-            </div>
-          </li>
+          </li> */}
           <li className={styles.flex}>
             <div className={styles.row}>
               <span className={styles.label}>自主评价：</span>
               <span className={styles.name}>
-                上课时希望老师打比方的例子更好，可以夸张点，最重要的还事通俗易懂吧。然后就是
-                老师后面放个小黑板。提前把课堂要求写在那以及把这堂课的章节几个简答题和名词解释题目
-                写上上课时希望老师多讲点打比方的例子更好，我要吐槽客服态度，最重要的还事通俗易懂然
-                就是老师后面放个小黑板。提前把课堂要求写在那以及把这堂课的章节几个简答题和名词解释
-                题目写上上课时希望老师多讲点打比方的例子更好，可以夸张点，最重要的还事通俗易懂吧。
-                然后就是老师后面放个
+                {item.opinion}
               </span>
             </div>
           </li>
