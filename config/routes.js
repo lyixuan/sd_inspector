e93c0c9c1c3a40e4e71faaf264f4b7c178abea81
Admin <admin@AdmindeMacBook-Pro-3.local>
2019-07-01 15:26:39 +0800

    错误代码

--- config/routes.js
+++ config/routes.js
@@ -283,18 +283,6 @@ export const routes = [
           }
         ],
       },
-      {
-        path: '/entrancePlatform',
-        name: '报考督学平台',
-        pageRedirect: '/entrancePlatform/userList',
-        routes: [
-          {
-            path: '/entrancePlatform/userList',
-            component: './ko/entrancePlatform/userList',
-            name: 'AI工作台',
-          }
-        ],
-      },
     ],
   },
 ];
