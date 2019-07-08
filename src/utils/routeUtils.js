1cfd6142c181cfc9b9ca168a6d9b9ee463708cc3
lyx <123>
2019-07-08 21:32:28 +0800

    sso

--- src/utils/routeUtils.js
+++ src/utils/routeUtils.js
@@ -6,8 +6,8 @@ export function redirectToLogin() {
 
 export function casLogout() {
   const { origin } = window.location;
-  const logoutUrl = `${origin}/ew/caslogout`;
-  const service = `service=${origin}?originPage=${origin}`;
+  const logoutUrl = `${origin}/ew/caslogout?`;
+  const service = `service=${origin}/tologin?originPage=${origin}`;
 
-  window.location.href = `${logoutUrl}?${service}`;
+  window.location.href = `${logoutUrl}${service}`;
 }
