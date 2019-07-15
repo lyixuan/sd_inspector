0b777567365864ca3c3e83324a6d9c05ee69cf94
Admin <admin@AdmindeMacBook-Pro-3.local>
2019-07-15 11:08:13 +0800

    ko日报域名修改

--- src/pages/ko/koDailyReport/index.js
+++ src/pages/ko/koDailyReport/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import jwt from 'jsonwebtoken'
 
-const METABASE_SITE_URL = "http://m-bd.ministudy.com";
+// const METABASE_SITE_URL = "http://m-bd.ministudy.com";
+const METABASE_SITE_URL = "http://s.bd.ministudy.com";
 const METABASE_SECRET_KEY = "361bab48a014bcfca3f2290216eea29d6842ba567a9a0f8f79c60cb3435bb8a9";
 const payload = {
   resource: { dashboard: 101 },
