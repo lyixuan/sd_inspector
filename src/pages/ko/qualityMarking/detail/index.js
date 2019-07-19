152ae2261c38a40e9095543fdfa7d2df95bffec7
Admin <admin@AdmindeMacBook-Pro-3.local>
2019-07-19 14:44:53 +0800

    构建调试m

--- src/pages/ko/qualityMarking/detail/index.js
+++ src/pages/ko/qualityMarking/detail/index.js
@@ -95,7 +95,7 @@ class AiDetail extends React.Component {
     }
   }
   render() {
-    console.log('jjjjjj')
+    console.log('mmmmmmm')
     let tabType = 1;
     if (this.state.type == 1) {
       tabType = 'IM';
@@ -121,7 +121,7 @@ class AiDetail extends React.Component {
             </div>
             <div className={styles.dataClassfy}>
               <div className={styles.headBar}>数据分类</div>
-              {/*<DataClassfy type={type} computedIdNew={this.computedId} id={this.state.id} params={JSON.parse(this.props.location.query.params).type} idList={this.props.idList} submitParam={this.state.submitParam}></DataClassfy>*/}
+              <DataClassfy type={type} computedIdNew={this.computedId} id={this.state.id} params={JSON.parse(this.props.location.query.params).type} idList={this.props.idList} submitParam={this.state.submitParam}></DataClassfy>
             </div>
           </div>
         </Spin>
