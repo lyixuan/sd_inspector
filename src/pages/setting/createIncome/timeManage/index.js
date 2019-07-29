6bc477a789be42d1a9e57725f9c624858989c291
lyx <123>
2019-07-29 11:54:25 +0800

    创收绩效model层

--- src/pages/setting/createIncome/timeManage/index.js
+++ src/pages/setting/createIncome/timeManage/index.js
@@ -6,9 +6,10 @@ import BIButton from '@/ant_components/BIButton';
 import BISelect from '@/ant_components/BISelect';
 import style from './style.less';
 import { BiFilter } from '@/utils/utils';
+
 const { Option } = BISelect;
 
-@connect(({ timeManage,loading }) => ({
+@connect(({ timeManage, loading }) => ({
   timeManage,
   loading: loading.models.examOrg,
 }))
@@ -16,29 +17,36 @@ class TimeManage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      beginDate:null,
-      endDate:null,
+      dayDownload: null,
+      monthDownload: null,
     };
   }
-  onFormChange = (value,vname)=>{
+
+  onFormChange = (value, vname) => {
     this.setState({
-      [vname]:value
+      [vname]: value,
     });
   };
+
   render() {
-  return (
+    const {beginDate,endDate,dayDownload,monthDownload} = this.props.timeManage;
+    return (
       <Spin spinning={false}>
         <div className={style.box}>
           <div className={style.title}>创收绩效时间管理</div>
-          <div  className={style.line}><span>开始日期：</span><BIDatePicker style={{ width: 230 }}  onChange={(val,valStr) => this.onFormChange(valStr, 'beginDate')}/></div>
-          <div className={style.line}><span>结束日期：</span><BIDatePicker style={{ width: 230 }}  onChange={(val,valStr) => this.onFormChange(valStr, 'endDate')}/>&nbsp;&nbsp;
+          <div className={style.line}><span>开始日期：</span><BIDatePicker style={{ width: 230 }}
+                                                                      onChange={(val, valStr) => this.onFormChange(valStr, 'beginDate')}/>
+          </div>
+          <div className={style.line}><span>结束日期：</span><BIDatePicker style={{ width: 230 }}
+                                                                      onChange={(val, valStr) => this.onFormChange(valStr, 'endDate')}/>&nbsp;&nbsp;
             <BIButton type="primary">保存</BIButton></div>
         </div>
 
         <div className={style.box}>
           <div className={style.title}>创收绩效底表下载</div>
-          <div  className={style.line}><span>日报下载：</span>
-            <BISelect style={{ width: 230 }} placeholder="请选择" value={this.state.qualityType} onChange={(val) => this.onFormChange(val, 'qualityType')}>
+          <div className={style.line}><span>日报下载：</span>
+            <BISelect style={{ width: 230 }} placeholder="请选择" value={dayDownload}
+                      onChange={(val) => this.onFormChange(val, 'dayDownload')}>
               {BiFilter('QUALITY_TYPE').map(item => (
                 <Option key={item.id}>
                   {item.name}
@@ -48,7 +56,8 @@ class TimeManage extends React.Component {
             &nbsp;&nbsp;
             <BIButton type="primary">下载</BIButton></div>
           <div className={style.line}><span>月报下载：</span>
-            <BISelect style={{ width: 230 }} placeholder="请选择" value={this.state.qualityType} onChange={(val) => this.onFormChange(val, 'qualityType')}>
+            <BISelect style={{ width: 230 }} placeholder="请选择" value={monthDownload}
+                      onChange={(val) => this.onFormChange(val, 'monthDownload')}>
               {BiFilter('QUALITY_TYPE').map(item => (
                 <Option key={item.id}>
                   {item.name}
@@ -58,7 +67,7 @@ class TimeManage extends React.Component {
             &nbsp;&nbsp;
             <BIButton type="primary">下载</BIButton>
           </div>
-          </div>
+        </div>
       </Spin>
     );
   }
