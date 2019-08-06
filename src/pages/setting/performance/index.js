3e88e3794c834e9f83658d4c4655a02e3c27120a
wenwanyu <wenwanyu@sunlands.com>
2019-08-06 19:43:01 +0800

    添加tab

--- src/pages/setting/performance/index.js
+++ src/pages/setting/performance/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Tabs } from 'antd';
+import BITabs from '@/ant_components/BITabs';
+
+// const { TabPane } = Tabs;
+const TabPane = BITabs.TabPane;
 
 class qualityAppeal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabType: 1,
+    };
+  }
 
+  callback = key => {
+    console.log(key);
+  };
+  onTabChange = val => {
+    console.log(val, 'val');
+  };
   render() {
-    return <div>1111</div>;
+    return (
+      <div>
+        <>
+          <div>
+            <BITabs
+              onChange={this.onTabChange}
+              defaultActiveKey={this.state.tabType}
+              animated={false}
+            >
+              <TabPane tab="在途质检申诉" key={1}>
+                <div>11111</div>
+              </TabPane>
+              <TabPane tab="结案质检申诉" key={2}>
+                <div>22222</div>
+              </TabPane>
+            </BITabs>
+          </div>
+        </>
+      </div>
+    );
   }
 }
 
