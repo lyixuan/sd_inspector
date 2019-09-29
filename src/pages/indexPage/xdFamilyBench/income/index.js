9ef2f7871c05088a5f8c572143caa9686dd86846
Admin <admin@AdmindeMacBook-Pro-4.local>
2019-09-29 15:35:20 +0800

    埋点

--- src/pages/indexPage/xdFamilyBench/income/index.js
+++ src/pages/indexPage/xdFamilyBench/income/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ColorBlock from '../../components/colorBlock';
+import TopTabs from '../../components/topTabs';
+import TableList from './components/list';
+import styles from './style.less';
 
 class Income extends React.Component {
   constructor(props) {
@@ -28,9 +31,18 @@ class Income extends React.Component {
       title1: '家族经流水',
       num: 20000
     }]
+    const tabParams = [{
+      name: '小组创收',
+      children: <TableList tabKey="1"/>
+    }, {
+      name: '班主任创收',
+      children: <TableList tabKey="2"/>
+    }]
     return (
-      <div>
+      <div className={styles.income}>
         <ColorBlock dataSet={arr} title='title1' num='num'/>
+        <div style={{height: '24px'}}></div>
+        <TopTabs  tabParams={tabParams}/>
       </div>
     );
   }
