314c0da4e927268db89aab2816ba38ed8adede0d
zhangshuli <zhangshuli8041@163.com>
2019-10-08 19:10:18 +0800

    page

--- src/pages/indexPage/services.js
+++ src/pages/indexPage/services.js
@@ -2,15 +2,15 @@ import request from '@/utils/request';
 
 // 班主任分层 - 本期创收 - 综合对比
 export async function getContrastIncomeKpiPkList(params) {
-    return request('/deskperfpcapi/workbenchIncomeKpi/incomeKpiPkItem', { method: 'get', params });
+  return request('/deskperfpcapi/workbenchIncomeKpi/incomeKpiPkItem', { method: 'get', params });
 }
 // 班主任分层 - 本期创收 - 查询对比小组
 export async function getIncomeKpiPkList(params) {
-    return request('/deskperfpcapi/workbenchIncomeKpi/getIncomeKpiPkList', { method: 'get', params });
+  return request('/deskperfpcapi/workbenchIncomeKpi/getIncomeKpiPkList', { method: 'get', params });
 }
 // 班主任分层 - 本期创收 - 个人信息
 export async function getIncomeKpiPersonInfo(params) {
-    return request('/deskperfpcapi/workbenchIncomeKpi/incomeKpiPersonInfo', { method: 'get', params });
+  return request('/deskperfpcapi/workbenchIncomeKpi/incomeKpiPersonInfo', { method: 'get', params });
 }
 
 // 班主任工作台 - 本期质检统计
@@ -37,11 +37,20 @@ export async function groupPkList(params) {
   return request('/deskperfpcapi/scorePk/group/pk', { method: 'POST', data: params })
 }
 //判断是否显示本期学分的模块
-export async function  isShowPermission(params) {
+export async function isShowPermission(params) {
   return request('/deskperfpcapi/scorePk/permission', { method: 'get', params })
 }
 
 // 绩效详情
 export async function getKpiInfo(params) {
   return request('/deskperfpcapi/workbenchIncomeKpi/getKpiInfo', { method: 'get', data: params })
+}
+
+// 工作台—绩效详情
+export async function getFamilykpiInfo(params) {
+  return request('/family/kpiInfo', { method: 'get', data: params })
+}
+//本期学分-明细
+export async function scoreDetail(params) {
+  return request('/family/score/detail', { method: 'get', data: params })
 }
--- src/pages/indexPage/xdFamilyBench/currentCredit/components/list.js
+++ src/pages/indexPage/xdFamilyBench/currentCredit/components/list.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { connect } from 'dva';
 import BITable from '@/ant_components/BITable';
 import styles from './style.less';
+import up from '@/assets/xdFamily/rankUp.png';
+import down from '@/assets/xdFamily/rankDown.png';
+import normal from '@/assets/xdFamily/rankNormal.png';
+import rank1 from '@/assets/xdFamily/rank1.png';
+import rank2 from '@/assets/xdFamily/rank2.png';
+import rank3 from '@/assets/xdFamily/rank3.png';
+import SmallProgress from '@/pages/indexPage/components/smallProgress'
 
 @connect(({ loading }) => ({
   loading: loading.effects['xdWorkModal/getIncomeKpiPkList'],
@@ -10,91 +17,251 @@ class ProfitList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      profitList: [],
+      // profitList: [],
+      profitList: [{
+        groupId: 1,
+        groupName: '小组名称1',
+        cpName: '运营长名称',
+        creditRankingCoefficient: '排名系数',
+        creditRanking: '排名',
+        rankingFlag: '1', //1降低 0持平 1上升
+        dimensionList: [{
+          "children": [{
+            "dimensionName": "正面均分",
+            "groupScore": "4.93",
+            "id": 2,
+            "myNum": 0,
+            "myScore": "11.81",
+            "myScoreRatio": "14.15",
+            "parentId": 1,
+            "unit": "",
+            "children": [{
+              "children": [{
+                "children": [],
+                "dimensionName": "有效直播",
+                "groupScore": "1.70",
+                "id": 37,
+                "myNum": 5049,
+                "myScore": "3.63",
+                "myScoreRatio": "6.80",
+                "parentId": 36,
+                "unit": "个"
+              }, {
+                "children": [],
+                "dimensionName": "有效重播",
+                "groupScore": "0.64",
+                "id": 38,
+                "myNum": 4023,
+                "myScore": "1.46",
+                "myScoreRatio": "49.41",
+                "parentId": 36,
+                "unit": "个"
+              }],
+              "dimensionName": "有效出勤",
+              "groupScore": "2.34",
+              "id": 36,
+              "myNum": 0,
+              "myScore": "5.09",
+              "myScoreRatio": "16.31",
+              "parentId": 2,
+              "unit": ""
+            }, {
+              "dimensionName": "有效做题",
+              "groupScore": "2.47",
+              "id": 39,
+              "myNum": 0,
+              "myScore": "5.19",
+              "myScoreRatio": "35.49",
+              "parentId": 2,
+              "unit": "",
+              children: []
+            }],
+          }],
+          "dimensionName": "学分均分",
+          "groupScore": "3.80",
+          "id": 1,
+          "myNum": 0,
+          "myScore": "11.16",
+          "myScoreRatio": "4.95",
+          "parentId": 0,
+          "unit": ""
+        }],
+      }, {
+        groupId: 1,
+        groupName: '小组名称1',
+        cpName: '运营长名称',
+        creditRankingCoefficient: '排名系数',
+        creditRanking: '排名',
+        rankingFlag: '1', //1降低 0持平 1上升
+        dimensionList: [{
+          "children": [{
+            "children": [{
+              "children": [{
+                "children": [],
+                "dimensionName": "有效直播",
+                "groupScore": "1.70",
+                "id": 37,
+                "myNum": 5049,
+                "myScore": "3.63",
+                "myScoreRatio": "6.80",
+                "parentId": 36,
+                "unit": "个"
+              }, {
+                "children": [],
+                "dimensionName": "有效重播",
+                "groupScore": "0.64",
+                "id": 38,
+                "myNum": 4023,
+                "myScore": "1.46",
+                "myScoreRatio": "49.41",
+                "parentId": 36,
+                "unit": "个"
+              }],
+              "dimensionName": "有效出勤",
+              "groupScore": "2.34",
+              "id": 36,
+              "myNum": 0,
+              "myScore": "5.09",
+              "myScoreRatio": "16.31",
+              "parentId": 2,
+              "unit": ""
+            }, {
+              "dimensionName": "有效做题",
+              "groupScore": "2.47",
+              "id": 39,
+              "myNum": 0,
+              "myScore": "5.19",
+              "myScoreRatio": "35.49",
+              "parentId": 2,
+              "unit": "",
+              children: []
+            }],
+            "dimensionName": "正面均分",
+            "groupScore": "4.93",
+            "id": 2,
+            "myNum": 0,
+            "myScore": "11.81",
+            "myScoreRatio": "14.15",
+            "parentId": 1,
+            "unit": ""
+          }],
+          "dimensionName": "学分均分",
+          "groupScore": "3.80",
+          "id": 1,
+          "myNum": 0,
+          "myScore": "11.16",
+          "myScoreRatio": "4.95",
+          "parentId": 0,
+          "unit": ""
+        }],
+      }]
     }
   }
   componentDidMount() {
-    this.getData();
+
+
   }
+  fillDataSource() {
+    const arr = [];
+
+    this.state.profitList.map(item => {
+      item.obj = {};
+      this.flatTree(item.dimensionList[0]).map(item2 => {
+        item.obj[item2.id] = item2
+      })
+    })
+
+    return this.state.profitList
+  }
+
+  flatTree({ id, dimensionName, myScore, children }, result = [], pid = "", level = 1) {
+    result = [{ id, dimensionName, myScore, pid, level }]
+    if (Array.isArray(children) && children.length) {
+      children.reduce((result, data) => {
+        console.log(177, result, data)
+        result.push(...this.flatTree(data, result, id, level + 1))
+        return result
+      }, result)
+    }
+    return result
+  }
+
   columns = () => {
     const columns = [
       {
         title: '集团排名',
-        dataIndex: 'sort',
-        key: 'sort',
+        dataIndex: 'creditRanking',
+        key: 'creditRanking',
+        render: (text, record) => {
+          console.log(196, record)
+          let src = null;
+          let className = '';
+          let rank = 1;
+          if (record.rankingFlag == 1) {
+            src = up
+          } else if (record.rankingFlag == 2) {
+            src = down
+          } else {
+            className = 'normal'
+            src = normal
+          }
+          if (record.rank == 1) {
+            rank = rank1
+          } else if (record.rank == 2) {
+            rank = rank2
+          } else if (record.rank == 3) {
+            rank = rank3
+          }
+          return (
+            <div className={`${styles.rankColumn} ${styles[className]}`}>
+              {text}<img className={styles.changes} src={src} />
+            </div>
+          )
+        },
+        width: '6.66%'
       }, {
         title: '小组',
-        dataIndex: 'org',
-        key: 'org',
-      }, {
-        title: this.props.tabKey === '1' ? '运营长' : '班主任',
-        dataIndex: 'userName',
-        key: 'userName',
+        dataIndex: 'groupName',
+        key: 'groupName',
+        width: '6.66%'
       }, {
         title: '运营长',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
+        dataIndex: 'cpName',
+        key: 'cpName',
+        width: '6.66%'
       }, {
         title: '排名系数',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }, {
-        title: '学分均分',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }, {
-        title: '正面均分',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }, {
-        title: '有效直播',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }, {
-        title: '有效重播',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }, {
-        title: '有效做题',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }, {
-        title: '社区运营',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }, {
-        title: '创收',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }, {
-        title: '负面均分',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }, {
-        title: '优新',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }, {
-        title: 'IM',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }, {
-        title: '质检',
-        dataIndex: 'incomeKpi',
-        key: 'incomeKpi',
-      }
+        dataIndex: 'creditRankingCoefficient',
+        key: 'creditRankingCoefficient',
+        width: '6.66%'
+      },
     ];
+    if (this.fillDataSource().length > 0) {
+      const arr = Object.values(this.fillDataSource()[0].obj)
+      arr.map(item => {
+        if (item.level >= 4) return;
+        columns.push({
+          title: item.dimensionName,
+          dataIndex: item.id,
+          key: item.id,
+          width: '6.67%',
+          className: item.level ? styles.bgColor : '',
+          render: (text, record) => {
+            console.log(310, record.obj[item.id])
+            if (record.obj[item.id].dimensionName == '正面均分') {
+              return <div>
+                <div>{record.obj[item.id].myScore}</div>
+                <SmallProgress isColor="green" percent="20%"></SmallProgress>
+              </div>
+            }
+            return <div className={record.obj[item.id].level ? styles.bgColor : ''}>{record.obj[item.id].myScore}</div>
+          }
+        })
+      })
+    }
+
     return columns || [];
   };
-  getData = () => {
-    this.props.dispatch({
-      type: 'xdWorkModal/getCountCurrentQuality',
-      payload: { params: { id: 1446 } },
-      callback: (profitList) => {
-        this.setState({ profitList: [{ id: 1, }, { id: 2 }] })
-      },
-    });
-  }
 
   render() {
     const { profitList = [] } = this.state;
