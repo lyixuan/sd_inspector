5140c4bea97a3d0550ea4000493865541096816f
lyx <123>
2019-10-09 11:00:33 +0800

    质检2

--- config/routes.js
+++ config/routes.js
@@ -136,15 +136,15 @@ export const routes = [
               path: '/qualityAppeal/qualityAppeal',
             },
           },
-          {
-            path: '/qualityAppeal/qualityAppeal/edit',
-            component: './qualityAppeal/qualityAppeal/edit',
-            name: '发起申诉',
-            bread: {
-              name: '质检申诉管理',
-              path: '/qualityAppeal/qualityAppeal',
-            },
-          },
+          // {
+          //   path: '/qualityAppeal/qualityAppeal/edit',
+          //   component: './qualityAppeal/qualityAppeal/edit',
+          //   name: '发起申诉',
+          //   bread: {
+          //     name: '质检申诉管理',
+          //     path: '/qualityAppeal/qualityAppeal',
+          //   },
+          // },
           {
             path: '/qualityAppeal/qualityAppeal/appeal',
             component: './qualityAppeal/qualityAppeal/appeal',
@@ -163,29 +163,29 @@ export const routes = [
               path: '/qualityAppeal/qualityAppeal',
             },
           },
-          {
-            path: '/qualityAppeal/qualityAppeal/create',
-            component: './qualityAppeal/qualityAppeal/create',
-            name: '申诉审核',
-            bread: {
-              name: '申诉审核',
-              path: '/qualityAppeal/qualityAppeal',
-            },
-          },
-          {
-            path: '/qualityAppeal/qualityBook',
-            component: './qualityAppeal/qualityBook',
-            name: '品控质检手册',
-          },
-          {
-            path: '/qualityAppeal/qualityBook/create',
-            component: './qualityAppeal/qualityBook/create',
-            name: '新建质检手册',
-            bread: {
-              name: '品控质检手册',
-              path: '/qualityAppeal/qualityBook',
-            },
-          },
+          // {
+          //   path: '/qualityAppeal/qualityAppeal/create',
+          //   component: './qualityAppeal/qualityAppeal/create',
+          //   name: '申诉审核',
+          //   bread: {
+          //     name: '申诉审核',
+          //     path: '/qualityAppeal/qualityAppeal',
+          //   },
+          // },
+          // {
+          //   path: '/qualityAppeal/qualityBook',
+          //   component: './qualityAppeal/qualityBook',
+          //   name: '品控质检手册',
+          // },
+          // {
+          //   path: '/qualityAppeal/qualityBook/create',
+          //   component: './qualityAppeal/qualityBook/create',
+          //   name: '新建质检手册',
+          //   bread: {
+          //     name: '品控质检手册',
+          //     path: '/qualityAppeal/qualityBook',
+          //   },
+          // },
         ],
       },
       {
