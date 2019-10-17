09163eec66a1aa6e6340f80f295605ce78dfd2b1
Admin <admin@AdmindeMacBook-Pro-4.local>
2019-10-17 16:46:29 +0800

    组件样式

---
+++ src/components/BIDrawer/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Drawer } from 'antd';
+import styles from './style.less';
+
+class BIDrawer extends React.Component {
+  onClose = () => {
+    const { onClose } = this.props;
+    if (onClose && typeof onClose === 'function') {
+      onClose();
+    }
+  }
+  onOpen = () => {
+    const { onOpen } = this.props;
+    if (onOpen && typeof onOpen === 'function') {
+      onOpen();
+    }
+  }
+  render() {
+    const { visible, drawerStyle = {} } = this.props;
+    return (
+      <div className={`${styles.BIDrawer} ${visible ? styles.BIDrawerOpen : ''}`}>
+        <div className={styles.openWrapper}>
+          <div onClick={this.onClose} class={styles.drawerMask}></div>
+          <div className={styles.drawer}>
+            <span onClick={this.onClose} className={styles.toggleClose}>收取PK小组 {'<'}</span>
+            <div className={styles.drawerBody} style={drawerStyle}>
+              {this.props.children}
+            </div>
+          </div>
+        </div>
+        {!visible ? <span onClick={this.onOpen} className={styles.toggleOpen}>收取PK小组 {'>'}</span> : ''}
+      </div>    
+    );
+  }
+}
+
+export default BIDrawer;
--- src/pages/indexPage/xdWorkbench/profit/index.js
+++ src/pages/indexPage/xdWorkbench/profit/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '../../components/container';
 import ProfitList from './components/list';
 import ProfitTabs from './components/tabs';
+import BIDrawer from '@/components/BIDrawer';
 
 class Profit extends React.Component {
   constructor(props) {
@@ -11,6 +12,7 @@ class Profit extends React.Component {
     this.state = {
       pkUser: pkUser ? Number(pkUser) : '', // 选中的pk者
       pkListType: pkListType ? Number(pkListType) : 5, // 列表选项--同级排行
+      visible: false
     }
   }
 
@@ -22,13 +24,27 @@ class Profit extends React.Component {
     localStorage.setItem('pkListType', v);
     this.setState({pkListType: v});
   }
+  toggleDrawer = (bul) => {
+    this.setState({
+      visible: bul,
+    });
+  };
 
   render() {
-    const { pkUser, pkListType } = this.state;
+    const { pkUser, pkListType, visible } = this.state;
     return (
-      <Container title='本期创收' propStyle={{ display: 'flex' }}>
+      <Container title='本期创收' propStyle={{ display: 'flex', position: 'relative' }}>
         <ProfitTabs {...this.props} pkUser={pkUser} pkListType={pkListType} />
-        <ProfitList {...this.props} pkUser={pkUser} pkListType={pkListType} changePkListType={this.changePkListType} changeSelected={this.changeSelected} />
+        <BIDrawer
+          placement="right"
+          closable={false}
+          onClose={() => this.toggleDrawer(false)}
+          onOpen={() => this.toggleDrawer(true)}
+          visible={visible}
+          getContainer={false}
+        >
+          <ProfitList {...this.props} pkUser={pkUser} pkListType={pkListType} changePkListType={this.changePkListType} changeSelected={this.changeSelected} />
+        </BIDrawer>
       </Container>
     );
   }
