ed9de315784334264eaf355a009dfadd2e63b4fe
zhangshuli <zhangshuli8041@163.com>
2019-10-18 14:43:45 +0800

    constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -24,7 +24,7 @@ export const STATIC_HOST = {
 
 export const SERVER_HOST = {
   production: 'http://bd.ministudy.com',
-  localhost: 'http://bd.ministudy.com',
+  localhost: 'http://test.xd.admin.ministudy.com',
   localhost2: 'http://test.xd.admin2.ministudy.com',
   development: 'http://test.xd.admin.ministudy.com',
   development2: 'http://test.xd.admin2.ministudy.com',
