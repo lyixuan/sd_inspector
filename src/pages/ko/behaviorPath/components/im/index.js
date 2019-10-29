651f6a8683566fe8d2e733f0f24b14b2be8cd315
zhangshuli <zhangshuli8041@163.com>
2019-10-29 20:01:21 +0800

    用户轨迹评论时间

--- src/pages/ko/behaviorPath/components/im/index.js
+++ src/pages/ko/behaviorPath/components/im/index.js
@@ -34,6 +34,7 @@ function Prise(props) {
   return (
     <li className={styles.step}>
       <div className={styles.time}>
+        {props.li.evaluateTime ? props.li.evaluateTime.split(' ')[1] : ''}
         {/* {props.li.countDate ? props.li.countDate.split(' ')[1] : ''} */}
       </div>
       <div className={styles.content}>
