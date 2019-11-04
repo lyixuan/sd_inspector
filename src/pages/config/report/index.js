b57621f0fa256107b92f393ffd725897ea7f82f4
wenwanyu <wenwanyu@sunlands.com>
2019-11-04 19:46:45 +0800

    report

--- src/pages/config/report/index.js
+++ src/pages/config/report/index.js
@@ -1,27 +1,135 @@
 import React from 'react';
 import { connect } from 'dva';
-import TimeManage from './timeManage/index';
-import styles from './style.less';
+import { Spin } from 'antd';
+import BIDatePicker from '@/ant_components/BIDatePicker';
+import BIButton from '@/ant_components/BIButton';
+import BISelect from '@/ant_components/BISelect';
+import style from './style.less';
+import moment from 'moment/moment';
+import { message, Input, Checkbox } from 'antd/lib/index';
+const { BIRangePicker } = BIDatePicker;
 
-@connect(({ createIncome, loading }) => ({
-  createIncome,
-  loading: loading.effects['createIncome/getArchiveList'],
+const { Option } = BISelect;
+
+@connect(({ report, loading }) => ({
+  report,
+  loadingTime: loading.effects['createIncome/getTimeRange'],
 }))
 class Report extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ignoreUsers: '',
+      title: '',
+    };
+  }
+
   componentDidMount() {
-    // 获取绩效包列表
+    // this.props.dispatch({
+    //   type: 'report/getIgnoreUser',
+    //   payload: { params: {} },
+    // });
+  }
+
+  onFormChange = (value, vname) => {
+    this.setState({
+      [vname]: value,
+    });
+  };
+
+  onTimeChange = (value, vname) => {
+    console.log(value, vname, 'onTimeChange');
+    // this.props.dispatch({
+    //   type: '/saveTime',
+    //   payload: { [vname]: value },
+    // });
+  };
+  onChange = checkedValues => {
+    console.log('checked = ', checkedValues);
+  };
+
+  changeTitle = () => {};
+
+  sendFn = () => {
     this.props.dispatch({
-      type: 'createIncome/getArchiveList',
+      type: 'report/send',
       payload: { params: {} },
     });
-  }
+  };
+
+  inputChange = (e, key) => {
+    const obj = {};
+    // const value = e.target.value;
+    // if (isNaN(Number(value)) && value !== '-') {
+    //   this.props.onError && this.props.onError();
+    //   return;
+    // }
+    obj[key] = e.target.value;
+    this.setState({ ...obj });
+    e.stopPropagation();
+  };
+
+  send = () => {
+    const { beginDate, endDate, title, roleList, ignoreUsers } = this.state;
+  };
+
   render() {
-    const { archiveList = [] } = this.props.createIncome || {};
-    const { loading } = this.props;
+    const { ignoreUsers, title } = this.state;
+    const options = [
+      { label: '院长&副院长', value: '1' },
+      { label: '家族长', value: '2' },
+      { label: '运营长', value: '3' },
+      { label: '班主任', value: '4' },
+      { label: '我自己', value: '5' },
+    ];
     return (
-      <div className={styles.createIncomeWrap}>
-        <TimeManage achievementList={archiveList} loading={loading} />
-      </div>
+      <>
+        <Spin spinning={false}>
+          <div className={style.box}>
+            <div className={style.title}>发周报</div>
+            <div className={style.line}>
+              <span>开始日期：</span>
+              <BIRangePicker
+                style={{ width: 330 }}
+                allowClear={false}
+                placeholder={['选择起始时间', '选择截止时间']}
+                // value={startDate ? moment(startDate) : undefined}
+                onChange={(val, valStr) => this.onTimeChange(val, valStr, 'startDate')}
+              />
+            </div>
+            <div className={style.line}>
+              <span>邮件标题：</span>
+              <Input
+                value={title}
+                onInput={e => this.inputChange(e, 'title')}
+                style={{ width: 330 }}
+              />
+            </div>
+            <div className={style.line}>
+              <span>发送范围：</span>
+              <Checkbox.Group
+                style={{ width: 440, height: ' 32px', lineHeight: '32px' }}
+                options={options}
+                defaultValue={['Apple']}
+                onChange={() => this.onChange}
+              />
+            </div>
+            <div className={style.line}>
+              <span>屏蔽用户：</span>
+              <Input
+                style={{ width: 440 }}
+                value={ignoreUsers}
+                onInput={e => this.inputChange(e, 'ignoreUsers')}
+              />
+            </div>
+            <div style={{ width: 440, textAlign: 'center' }}>
+              <BIButton type="primary" onClick={this.sendFn}>
+                发送
+              </BIButton>
+            </div>
+          </div>
+        </Spin>
+      </>
     );
   }
 }
--- src/pages/config/report/services.js
+++ src/pages/config/report/services.js
@@ -6,29 +6,11 @@ export async function getArchiveList() {
 }
 
 // 获取存档历史记录列表
-export async function getBatchLogList() {
-  return request('/incomeOrder/batchLogList');
+export async function getIgnoreUser() {
+  return request('/weekReport/initLastIgnoreUsers');
 }
 
 // 添加存档
-export async function saveBatchLog(data) {
-  return request('/incomeOrder/saveBatchLog', { method: 'post', data });
+export async function send(data) {
+  return request('/weekReport/sendMail', { method: 'post', data });
 }
-
-// 取消存档
-export async function cacelBatchLog(data) {
-  return request('/incomeOrder/cacelBatchLog', { method: 'post', data });
-}
-
-export async function getTimeRange() {
-  return request('/incomeOrder/dateRangeList');
-}
-
-export async function updateTimeRange(data) {
-  return request('/incomeOrder/updateDateRange', { method: 'post', data });
-}
-// 日报月报下载
-export async function reportExcelDownload(data) {
-  return request('/incomeOrder/reportExcelDownload', { method: 'post', data,responseType:'blob',getResponse: true  });
-}
-
