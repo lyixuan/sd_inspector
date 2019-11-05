b3330f82677a6ff9e09fa9f63b3811df19ba5ef3
Admin <admin@AdmindeMacBook-Pro-5.local>
2019-11-05 17:41:57 +0800

    去除测试

--- src/utils/request.js
+++ src/utils/request.js
@@ -75,13 +75,13 @@ const request = extend({
 request.interceptors.request.use((url, options) => {
   options.headers = Object.assign({}, options.headers, { 'X-Requested-With': 'XMLHttpRequest', authorization: storage.getToken() });
   const hasSelfPri = url.indexOf('/apis') > -1 || url.indexOf('/shinecollege') > -1 || url.indexOf('/inspectorapis') > -1 || url.indexOf('/deskperfpcapi') > -1;
-  const hasTest = url.includes('/test');
-  if (hasTest) {
-    return {
-      url: `http://172.16.29.154:8086${url.replace('/test', '')}`,
-      options,
-    };
-  } 
+  // const hasTest = url.includes('/test');
+  // if (hasTest) {
+  //   return {
+  //     url: `http://172.16.29.154:8086${url.replace('/test', '')}`,
+  //     options,
+  //   };
+  // } 
   return {
     url: `${SERVER_HOST}${PROXY_PATH(hasSelfPri)}${url}`,
     options,
