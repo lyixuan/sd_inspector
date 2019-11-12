75f6cd6e74eb2732c6a7253afd3702050c539a7b
lyx <123>
2019-11-12 19:41:06 +0800

    只保留邮件跳到指定菜单

--- src/utils/routeUtils.js
+++ src/utils/routeUtils.js
@@ -6,19 +6,37 @@ export function redirectToLogin() {
   storage.removeItem('admin_user');
   storage.removeItem('admin_auth');
   localStorage.clear();
-  const { origin,pathname } = window.location;
+  const { origin,pathname,search } = window.location;
   const serverUrl = `${CAS_HOST}/tologin`;
-  window.location.href = `${serverUrl}?originPage=${origin}${pathname}`;
+  let fromEmail = false;
+  if(search){
+    fromEmail = isFromEmail(search);
+  }
+  if(fromEmail) {
+    window.location.href = `${serverUrl}?originPage=${origin}${pathname}`;
+  } else {
+    window.location.href = `${serverUrl}?originPage=${origin}`;
+  }
 }
 
 export function casLogout() {
   storage.removeItem('admin_user');
   storage.removeItem('admin_auth');
   localStorage.clear();
-  const { origin,pathname } = window.location;
+  const { origin,pathname,search } = window.location;
   const logoutUrl = `${CAS_HOST}/apis/caslogout?`;
-  const pageUrl = `pageUrl=${CAS_HOST}/tologin?originPage=${origin}${pathname}`;
-  window.location.href = `${logoutUrl}${pageUrl}`;
+  let fromEmail = false;
+  if(search){
+    fromEmail = isFromEmail(search);
+  }
+  console.log('fromEmail',fromEmail)
+  if(fromEmail) {
+    const pageUrl = `pageUrl=${CAS_HOST}/tologin?originPage=${origin}${pathname}`;
+    window.location.href = `${logoutUrl}${pageUrl}`;
+  } else {
+    const pageUrl = `pageUrl=${CAS_HOST}/tologin?originPage=${origin}`;
+    window.location.href = `${logoutUrl}${pageUrl}`;
+  }
 }
 export function checkPathname(path = '') {
   const data1 = storage.getUserAuth() || [];
@@ -27,4 +45,17 @@ export function checkPathname(path = '') {
   if (menuKey) {
     return true;
   } else return false;
+}
+
+function isFromEmail(search) {
+  let result  = false;
+  const str = search.substr(1);
+  const arr = str.split('&');
+  arr.forEach((v)=>{
+    const arrItem = v.split('=');
+    if(arrItem[0] && arrItem[0]==='src' && arrItem[1] && arrItem[1]==='email') {
+      result = true;
+    }
+  });
+  return result;
 }
