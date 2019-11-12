3ecdbab097b9027e8509e48492f03a339642d2db
lyx <123>
2019-11-12 21:44:01 +0800

    处理fromEmail参数

--- src/utils/routeUtils.js
+++ src/utils/routeUtils.js
@@ -47,5 +47,6 @@ export function checkPathname(path = '') {
 }
 
 function isFromEmail(pathname) {
+  alert(pathname);
   return pathname.indexOf('/fromEmail')>0?pathname.indexOf('/fromEmail'):false;
 }
