ce6b12f5be5cccce826aa9ad936ebfb2d3e622e5
lyx <123>
2019-11-14 10:24:31 +0800

    成绩组件

--- src/pages/ko/behaviorPath/components/score/ScoreItem.js
+++ src/pages/ko/behaviorPath/components/score/ScoreItem.js
@@ -3,20 +3,125 @@ import { Icon } from 'antd';
 import styles from './style.css';
 
 export default class ScoreItem extends React.Component {
-  render() {
-    const { listItem = {},index=0,collapse=false}  = this.props;
+
+  renderItem = (examDetailList) => {
+    return examDetailList.length > 3 ? this.renderGt3(examDetailList) : this.renderLt3(examDetailList);
+  };
+
+  renderLt3 = (examDetailList) => {
+    const ln = examDetailList.length;
+    return (
+      <div className={styles.itemContent1}>
+        {ln > 0 ? <div>
+          <div className={`${examDetailList[0].score < 60 ? styles.redText : ''}`}>{examDetailList[0].score}</div>
+          <div>{examDetailList[0].examName}</div>
+        </div> : null}
+        {ln > 1 ? <div className={styles.row1Line}/> : null}
+        {ln > 1 ? <div>
+          <div className={`${examDetailList[1].score < 60 ? styles.redText : ''}`}>{examDetailList[1].score}</div>
+          <div>{examDetailList[1].examName}</div>
+        </div> : null}
+        {ln > 2 ? examDetailList[2] ? <div className={styles.row1Line}/> : <div></div> : null}
+        {ln > 1 ? <div>
+          <div
+            className={`${examDetailList[2] && examDetailList[2].score < 60 ? styles.redText : ''}`}>{examDetailList[2] && examDetailList[2].score}</div>
+          <div>{examDetailList[2] && examDetailList[2].examName}</div>
+        </div> : null}
+      </div>
+    );
+  };
+  renderGt3 = (examDetailList) => {
+    const ln = examDetailList.length;
+    const times = Math.ceil(ln / 3);
+    const last1Index = (times - 1) * 3;
+    const last2Index = (times - 1) * 3 + 1;
+    const last3Index = (times - 1) * 3 + 2;
+    const last = ln % 3;
+    const middleList = ln>6?examDetailList.slice(2,examDetailList.length-last):[];
+    const middleRow = middleList.length/3;
+    let middleDiv = [];
+    if (middleRow >= 1) {
+      for (let i = 0; i < middleRow;i++){
+        middleDiv.push(
+          <div className={`${styles.row} ${styles.row2} ${styles.row3}`}>
+            <div>
+              <div
+                className={`${examDetailList[3*(i+1)+1] && examDetailList[3*(i+1)+1].score < 60 ? styles.redText : ''}`}>{examDetailList[3*(i+1)+1] && examDetailList[3*(i+1)+1].score}</div>
+              <div>{examDetailList[3*(i+1)+1] && examDetailList[3*(i+1)+1].examName}</div>
+            </div>
+            <div className={styles.row1Line}/>
+            <div>
+              <div
+                className={`${examDetailList[3*(i+1)+2] && examDetailList[3*(i+1)+2].score < 60 ? styles.redText : ''}`}>{examDetailList[3*(i+1)+2] && examDetailList[3*(i+1)+2].score}</div>
+              <div>{examDetailList[3*(i+1)+2] && examDetailList[3*(i+1)+2].examName}</div>
+            </div>
+            <div className={styles.row1Line}/>
+            <div>
+              <div
+                className={`${examDetailList[3*(i+1)+3] && examDetailList[3*(i+1)+3].score < 60 ? styles.redText : ''}`}>{examDetailList[3*(i+1)+3] && examDetailList[3*(i+1)+3].score}</div>
+              <div>{examDetailList[3*(i+1)+3] && examDetailList[3*(i+1)+3].examName}</div>
+            </div>
+          </div>
+        )
+      }
+    }
 
     return (
-      <>
-        <div className={styles.itemBar} onClick={()=>this.props.clickBar(index)}>
-          <div><span className={styles.titleLeft}>{listItem.examDate}</span><span className={styles.titleRight}>{listItem.examCount}科</span></div><Icon type={collapse ? 'up' : 'down'} />
+      <div className={styles.itemContent2}>
+        <div className={`${styles.row} ${styles.row1}`}>
+          <div>
+            <div
+              className={`${examDetailList[0] && examDetailList[0].score < 60 ? styles.redText : ''}`}>{examDetailList[0] && examDetailList[0].score}</div>
+            <div>{examDetailList[0] && examDetailList[0].examName}</div>
+          </div>
+          <div className={styles.row1Line}/>
+          <div>
+            <div
+              className={`${examDetailList[1] && examDetailList[1].score < 60 ? styles.redText : ''}`}>{examDetailList[1] && examDetailList[1].score}</div>
+            <div>{examDetailList[1] && examDetailList[1].examName}</div>
+          </div>
+          <div className={styles.row1Line}/>
+          <div>
+            <div
+              className={`${examDetailList[2] && examDetailList[2].score < 60 ? styles.redText : ''}`}>{examDetailList[2] && examDetailList[2].score}</div>
+            <div>{examDetailList[2] && examDetailList[2].examName}</div>
+          </div>
         </div>
-        {collapse?<div className={styles.itemContent}>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-        </div>:null}
+        {middleDiv}
+        <div className={`${styles.row} ${styles.row2}`}>
+          <div>
+            <div
+              className={`${examDetailList[last1Index] && examDetailList[last1Index].score < 60 ? styles.redText : ''}`}>{examDetailList[last1Index] && examDetailList[last1Index].score}</div>
+            <div>{examDetailList[last1Index] && examDetailList[last1Index].examName}</div>
+          </div>
+          <div className={styles.row1Line}/>
+          <div>
+            <div
+              className={`${examDetailList[last2Index] && examDetailList[last2Index].score < 60 ? styles.redText : ''}`}>{examDetailList[last2Index] && examDetailList[last2Index].score}</div>
+            <div>{examDetailList[last2Index] && examDetailList[last2Index].examName}</div>
+          </div>
+          <div className={styles.row1Line}/>
+          <div>
+            <div
+              className={`${examDetailList[last3Index] && examDetailList[last3Index].score < 60 ? styles.redText : ''}`}>{examDetailList[last3Index] && examDetailList[last3Index].score}</div>
+            <div>{examDetailList[last3Index] && examDetailList[last3Index].examName}</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  render() {
+    const { listItem = {}, index = 0, collapse = false } = this.props;
+    const { examDetailList = [] } = listItem || {};
+    return (
+      <>
+        <div className={styles.itemBar} onClick={() => this.props.clickBar(index)}>
+          <div><span className={styles.titleLeft}>{listItem.examDate}</span><span
+            className={styles.titleRight}>{listItem.examCount}科</span></div>
+          <Icon type={collapse ? 'up' : 'down'}/>
+        </div>
+        {collapse ? this.renderItem(examDetailList) : null}
       </>
     );
   }
--- src/pages/ko/behaviorPath/index.js
+++ src/pages/ko/behaviorPath/index.js
@@ -242,38 +242,146 @@ class BehaviorPath1 extends React.Component {
     const scoreData = {
       list: [
         {
+          examDate:"201910",
+          examCount:7,
+          examDetailList:[{
+            examName:"市场及市场营销",score:11,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"JAVA程序设计",score:22,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"模拟电路",score:33,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"数字电路",score:44,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"大学英语",score:55,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"数字电路",score:66,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"模拟电路",score:77,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"数字电路",score:88,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"大学英语",score:99,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"数字电路",score:60,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"大学英语",score:70,
+            scoreDesc:"aute aliquip qui"
+          }]
+        },
+        {
+          examDate:"201910",
+          examCount:7,
+          examDetailList:[{
+            examName:"市场及市场营销",score:1,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"JAVA程序设计",score:2,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"模拟电路",score:3,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"数字电路",score:4,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"大学英语",score:5,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"数字电路",score:6,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"大学英语",score:7,
+            scoreDesc:"aute aliquip qui"
+          }]
+        },
+
+        {
+          examDate:"202004",
+          examCount:6,
+          examDetailList:[{
+            examName:"市场及市场营销",score:1,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"JAVA程序设计",score:2,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"模拟电路",score:3,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"数字电路",score:4,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"大学英语",score:5,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"毛概",score:6,
+            scoreDesc:"aute aliquip qui"
+          }]
+        },{
+          examDate:"201910",
+          examCount:5,
+          examDetailList:[{
+            examName:"市场及市场营销",score:1,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"JAVA程序设计",score:2,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"模拟电路",score:3,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"数字电路",score:4,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"大学英语",score:5,
+            scoreDesc:"aute aliquip qui"
+          }]
+        },
+        {
+          examDate:"201804",
+          examCount:3,
+          examDetailList:[{
+            examName:"市场及市场营销",score:79,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"毛概",score:28,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"中国近现代史",score:22,
+            scoreDesc:"aute aliquip qui"
+          }]
+        },
+        {
+          examDate:"201810",
+          examCount:2,
+          examDetailList:[{
+            examName:"高等数学",score:99,
+            scoreDesc:"aute aliquip qui"
+          },{
+            examName:"大学物理",score:44,
+            scoreDesc:"aute aliquip qui"
+          }]
+        },
+        {
           examDate:"201904",
           examCount:1,
           examDetailList:[{
             examName:"语文",score:28,
             scoreDesc:"aute aliquip qui"
             }]
-          },
-        {
-          examDate:"201910",
-          examCount:3,
-          examDetailList:[{
-            examName:"数学",score:79,
-            scoreDesc:"aute aliquip qui"
-          },{
-            examName:"语文",score:28,
-            scoreDesc:"aute aliquip qui"
-          },{
-            examName:"化学",score:22,
-            scoreDesc:"aute aliquip qui"
-          },{
-            examName:"英语",score:99,
-            scoreDesc:"aute aliquip qui"
-          }]
-        },
-        {
-          examDate:"202004",
-          examCount:3,
-          examDetailList:[{
-            examName:"英语",score:99,
-            scoreDesc:"aute aliquip qui"
-          }]
-        }],
+          }
+        ],
       pageNum:1,total:20,pages:10};
     return (
       <>
