1adab0df12943b73be5bf9721548668fbb194ae6
Admin <admin@AdmindeMacBook-Pro-6.local>
2019-11-20 18:35:51 +0800

    问卷调差

--- src/pages/questionnaire/components/index.js
+++ src/pages/questionnaire/components/index.js
@@ -4,10 +4,14 @@ import styles from './style.less';
 
 class QContainer extends React.Component {
   render() {
+    const { propsStyle = {}, children, ...props } = this.props;
     return (
-      <div className={styles.qContainer}>
+      <div className={styles.qContainer} {...props}>
           <div className={styles.title}>
             <img src={round} alt=''/>
+          </div>
+          <div style={propsStyle}>
+            {children}
           </div>
       </div>
     );
--- src/pages/questionnaire/index.js
+++ src/pages/questionnaire/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
 import BIScrollbar from '@/ant_components/BIScrollbar';
+import BIButton from '@/ant_components/BIButton';
 import head from '@/assets/questionnaire/head.png';
 import QContainer from './components';
 import styles from './style.less';
@@ -15,19 +16,32 @@ class Questionnaire extends React.Component {
     // this.props.dispatch({
     //   type: 'xdWorkModal/getUserInfo',
     // });
-    document.body.style.overflow = 'hidden'
+    // document.body.style.overflow = 'hidden'
   }
   render() {
     return (
       <>
         {
-          true && 
+          false && 
           <div className={styles.questionnaire}>
             <BIScrollbar style={{ width: '100%', height: '100%' }}>
               <div className={styles.modalContainer}>
                 <div className={styles.modal}>
                   <img className={styles.headImg} src={head} alt=''/>
-                  <QContainer></QContainer>
+                  <div style={{margin: '0 24px'}}>
+                    <QContainer propsStyle={{height: 340}}>
+
+                    </QContainer>
+                    <QContainer propsStyle={{height: 256}}>
+
+                    </QContainer>
+                    <QContainer propsStyle={{height: 254}}>
+
+                    </QContainer>
+                  </div>
+                  <div>
+                    <BIButton></BIButton>
+                  </div>
                 </div>
               </div>     
             </BIScrollbar>
