baf04ac3a725fe33a45787def9f95e5df37a15a1
lyx <123>
2019-11-20 20:03:05 +0800

    文件夹

---
+++ src/pages/cubePlan/home/model.js
@@ -0,0 +1,82 @@
+import { message } from 'antd/lib/index';
+import { getList, delelte, addData,updateData,getCourseTypeChildren,sortData } from './services';
+import { msgF } from '@/utils/utils';
+
+export default {
+  namespace: 'course',
+
+  state: {
+    dataList: [],
+    page: {},
+    sortList:[]
+  },
+
+  effects: {
+    *getList({ payload }, { call, put }) {
+      const params = payload.params;
+      const result = yield call(getList, params);
+      if (result.code === 20000) {
+        const dataList = result.data.list ? result.data.list : [];
+        const page = { total: result.data.total ? result.data.total : 0, pageNum: result.data.pageNum ? result.data.pageNum : 1 };
+        yield put({ type: 'save', payload: { dataList, page } });
+      } else {
+        message.error(msgF(result.msg,result.msgDetail));
+      }
+    },
+    *delelte({ payload }, { call }) {
+      const params = payload.params;
+      const result = yield call(delelte, params);
+      if (result.code === 20000) {
+        message.success('删除成功');
+      } else {
+        message.error(msgF(result.msg,result.msgDetail));
+      }
+    },
+    *addData({ payload }, { call, put }) {
+      const response = yield call(addData, payload);
+      if (response.code === 20000) {
+        message.success('保存成功');
+        return true
+      } else {
+        message.error(msgF(response.msg,response.msgDetail))
+      }
+    },
+    *updateData({ payload }, { call, put }) {
+      const response = yield call(updateData, payload);
+      if (response.code === 20000) {
+        message.success('保存成功');
+        return true
+      } else {
+        message.error(msgF(response.msg,response.msgDetail))
+      }
+    },
+    *getCourseTypeChildren({ payload }, { call, put }) {
+      const result = yield call(getCourseTypeChildren, payload);
+      if (result.code === 20000) {
+        const sortList = result.data ? result.data : [];
+        return sortList.sort((a, b) => a.sort - b.sort);
+      } else {
+        message.error(msgF(result.msg,result.msgDetail));
+      }
+    },
+    *sortData({ payload }, { call, put }) {
+      const response = yield call(sortData, payload);
+      if (response.code === 20000) {
+        message.success('保存成功');
+        return true
+      } else {
+        message.error(msgF(response.msg,response.msgDetail))
+      }
+    },
+  },
+  reducers: {
+    save(state, action) {
+      return { ...state, ...action.payload };
+    },
+
+  },
+
+  subscriptions: {
+  },
+};
+
---
+++ src/pages/cubePlan/model.js
@@ -0,0 +1,50 @@
+import { message } from 'antd';
+import {
+  getCollegeList,
+  getCourseType
+} from './services';
+import { msgF } from '@/utils/utils';
+
+export default {
+  namespace: 'cubePlan',
+
+  state: {
+    collegeList: [],
+    courseList: [],
+  },
+
+  effects: {
+    *getCollegeList({ payload }, { call, put }) {
+      const result = yield call(getCollegeList, {payload});
+      const collegeList = result.data || [];
+      if (result.code === 20000) {
+        yield put({ type: 'save', payload: { collegeList } });
+      } else {
+        message.error(msgF(result.msg,result.msgDetail));
+      }
+    },
+
+    *getCourseType({ payload }, { call, put }) {
+      const result = yield call(getCourseType, {payload});
+      const courseList = result.data || [];
+      courseList.forEach((item)=>{
+        item.children.forEach((v)=>{
+          delete v.children;
+        });
+      });
+      if (result.code === 20000) {
+        yield put({ type: 'save', payload: { courseList } });
+      } else {
+        message.error(msgF(result.msg,result.msgDetail));
+      }
+    },
+  },
+
+  reducers: {
+    save(state, { payload }) {
+      return { ...state, ...payload };
+    },
+  },
+
+  subscriptions: {},
+};
