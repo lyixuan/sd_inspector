eb99be2b0ffd2c4817c9c8a04a305f184f60809b
lyx <123>
2019-11-29 15:58:38 +0800

    质检报告

---
+++ src/pages/qualityReport/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'dva';
+
+@connect(({ cubePlan }) => ({
+  cubePlan,
+}))
+class CubePlan extends React.Component {
+  componentDidMount() {
+    this.getScreenWidth();
+  }
+  getScreenWidth = ()=>{
+    let screenRange = 'small_screen';
+    const sWidth = window.screen.width;
+
+    if(sWidth < 1440){
+      // width:1030px;
+      screenRange = 'small_screen';
+    } else if (sWidth >= 1440) {
+      // width:1190px;
+      screenRange = 'middle_screen';
+    }
+    this.props.dispatch({
+      type: 'cubePlan/checkScreen',
+      payload: { screenRange },
+    })
+  };
+  render() {
+    return <div>{this.props.children}</div>;
+  }
+}
+
+export default CubePlan;
