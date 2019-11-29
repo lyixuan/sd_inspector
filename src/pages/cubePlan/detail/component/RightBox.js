ed2eac2aea568f361c1e0e0e212d1301b3010fad
lyx <123>
2019-11-29 16:03:12 +0800

    bug

--- src/pages/cubePlan/detail/component/RightBox.js
+++ src/pages/cubePlan/detail/component/RightBox.js
@@ -158,7 +158,7 @@ class RightBox extends React.Component {
                                           onCopy={() => this.copySuccess('复制链接','魔方计划/魔方计划列表/')}>
               <span><img src={btnfz} alt=""/></span>
             </CopyToClipboard>}
-          {/*{usedH5===1&&<img src={btnewm} alt="" onClick={()=>this.openEwmModal('下载二维码','魔方计划/魔方计划列表/')}/>}*/}
+          {usedH5===1&&<img src={btnewm} alt="" onClick={()=>this.openEwmModal('下载二维码','魔方计划/魔方计划列表/')}/>}
           </Spin>
         </div>
 
