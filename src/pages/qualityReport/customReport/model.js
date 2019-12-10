db096cdde68dce509b5ccd59264dcebe9180a77e
liuyixuan <liuyixuan02@sunlands.com>
2019-12-10 17:37:30 +0800

    1.8 质检

--- src/pages/qualityReport/customReport/model.js
+++ src/pages/qualityReport/customReport/model.js
@@ -1,6 +1,3 @@
-import { message } from 'antd/lib/index';
-import { getDetail } from './services';
-import { msgF } from '@/utils/utils';
 
 export default {
   namespace: 'customReport',
@@ -9,16 +6,6 @@ export default {
   },
 
   effects: {
-    *getCubeDetail({ payload }, { call, put }) {
-      const params = payload.params;
-      const result = yield call(getDetail, params);
-      if (result.code === 20000) {
-        const detailInfo = result.data||{};
-        yield put({ type: 'save', payload: { detailInfo } });
-      } else {
-        message.error(msgF(result.msg,result.msgDetail));
-      }
-    },
   },
   reducers: {
     save(state, action) {
