66b8703680d889bb715f99522c0aeee74892c9d7
seven <756465934@qq.com>
2019-12-10 16:42:40 +0800

    修改请求小程序二维码测试服务器的地址

--- src/pages/cubePlan/constants/miniProgramHost.js
+++ src/pages/cubePlan/constants/miniProgramHost.js
@@ -1,7 +1,7 @@
 export const BASE_URL = {
   production: 'https://api.commeal.cn',
-  localhost: 'http://172.16.109.204:9200', // 鲲龙本地
-  // localhost: 'http://172.16.109.87:9200',
+  // localhost: 'http://172.16.109.204:9200', // 鲲龙本地
+  localhost: 'http://172.16.109.87:9200',
   localhost2: 'http://172.16.109.87:9200',
   development: 'http://172.16.109.87:9200',
   development2: 'http://172.16.109.87:9200'
