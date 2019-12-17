e4ae4b9e6a7f68ee773ca3b7f313fdbc44a5fd52
liuyixuan <liuyixuan02@sunlands.com>
2019-12-17 14:25:07 +0800

    2.0

--- src/pages/indexPage/component2/Income.js
+++ src/pages/indexPage/component2/Income.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import stylefather from '../indexPage.less';
+import chuangshou from '@/assets/newIndex/chuangshou@2x.png';
+import gengduo from '@/assets/newIndex/gengduo@2x.png';
 import style from './style.less';
 
 class Income extends React.Component {
@@ -7,7 +9,11 @@ class Income extends React.Component {
     const { WorkbenchScore } = this.props;
     return (
       <div className={stylefather.boxRight}>
-
+        <div className={stylefather.boxHeader}>
+          <img src={chuangshou} alt=""/>
+          <span className={stylefather.headerTitle}>创收</span>
+          <img src={gengduo} alt=""/>
+        </div>
       </div>
     );
   }
--- src/pages/indexPage/component2/Score.js
+++ src/pages/indexPage/component2/Score.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import stylefather from '../indexPage.less';
+import xuefen from '@/assets/newIndex/xuefen@2x.png';
+import gengduo from '@/assets/newIndex/gengduo@2x.png';
 import style from './style.less';
 
 class Score extends React.Component {
@@ -7,7 +9,12 @@ class Score extends React.Component {
     const { WorkbenchScore } = this.props;
     return (
       <div className={stylefather.boxLeft}>
-        <div className={stylefather.boxHeader}></div>
+        <div className={stylefather.boxHeader}>
+          <img src={xuefen} alt=""/>
+          <span className={stylefather.headerTitle}>学分</span>
+          <img src={gengduo} alt=""/>
+        </div>
+
       </div>
     );
   }
