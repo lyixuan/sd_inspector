f2300f34277948f7ff7fadf109122105967996e6
wenwanyu <wenwanyu@sunlands.com>
2019-12-20 04:44:28 +0800

    修改样式

--- src/pages/indexPage/component2/getImOptions.js
+++ src/pages/indexPage/component2/getImOptions.js
@@ -89,7 +89,7 @@ export function getOption(data) {
         name: 'IM',
         type: 'pie',
         center: ['50%', '49%'],
-        radius: ['36%', '58%'],
+        radius: ['39%', '62%'],
         clockwise: false, //饼图的扇区是否是顺时针排布
         avoidLabelOverlap: false,
         label: {
