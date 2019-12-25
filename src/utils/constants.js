2f7449ff7c401ec8c218c89ea256e6893a543cd4
seven <756465934@qq.com>
2019-12-25 14:50:53 +0800

    修改样式，提交代码

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -73,7 +73,7 @@ export const PROXY_PATH = hasSelfPri => {
 
 // 下载中心服务器地址
 export const DOWNLOAD_HOST = {
-  production: 'http://sscp.ministudy.com/college_learn/#/sessionRecord',
+  production: '',
   localhost: 'http://172.16.109.87:38083',
   localhost2: 'http://172.16.109.87:38083',
   development: 'http://172.16.109.87:38083',
