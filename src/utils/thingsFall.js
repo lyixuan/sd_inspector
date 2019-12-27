2ebed1a172d6dd838814745f178c03c035a99003
seven <756465934@qq.com>
2019-12-27 10:28:22 +0800

    提交物品掉落的代码

--- src/utils/thingsFall.js
+++ src/utils/thingsFall.js
@@ -1,3 +1,9 @@
+let myCanvas = document.createElement('canvas');
+let ctx = myCanvas.getContext('2d');
+let myImage = new Image();
+let fps = 60;
+let MAX_SPEED_X, MAX_SPEED_Y, G;
+
 class something {
   constructor(props) {
     this.x = props.x;
@@ -12,22 +18,52 @@ class something {
   updata = () => {
     let rotateDeg = Math.random() * 0.6 + 0.2;
     this.speedX += this.initSpeedX;
+    if (this.speedX >= MAX_SPEED_X || this.speedX <= -MAX_SPEED_X) {
+      this.initSpeedX *= -1;
+    }
+    if (this.speedY < MAX_SPEED_Y) {
+      this.speedY += G;
+    }
+    this.deg += rotateDeg;
+    this.x += this.speedX;
+    this.y += this.speedY;
+  };
+
+  draw = () => {
+    const radius = this.radius;
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.deg * Math.PI / 180);
+    ctx.drawImage(myImage, -radius, -radius, radius * 2, radius * 2);
+    ctx.restore();
   }
 }
 class ThingsFall {
   constructor(props) {
     this.things = [];
-    this.G = props.G || 0.01;
-    this.fps = 60;
-    this.MAX_SPEED_X = props.maxSpeedX || 1;
-    this.MAX_SPEED_Y = props.maxSpeedY || 1;
     this.canvasWidth = props.width || window.innerWidth;
     this.canvasHeight = props.height || window.innerHeight;
     this.addThingsInterval = props.interval || 150;
     this.recordTime = 0;
     this.nowTime = Date.now();
-    this.thingsImage = props.image;
-    this.myCanvas = document.createElement('canvas');
-    this.ctx = this.myCanvas.getContext('2d');
+    myImage.src = props.image;
+    MAX_SPEED_X = props.maxSpeedX || 1;
+    MAX_SPEED_Y = props.maxSpeedY || 1;
+    G = props.G || 0.01;
   }
+  init = () => {
+    window.requestAnimationFrame = window.requestAnimationFrame
+      || window.webkitRequestAnimationFrame
+      || window.mozRequestAnimationFrame
+      || window.oRequestAnimationFrame
+      || window.msRequestAnimationFrame
+      || function(callback) {
+        setTimeout(callback, 1000 / fps);
+      };
+    myCanvas.width = this.canvasWidth;
+    myCanvas.height = this.canvasHeight;
+    myCanvas.style.cssText = 'position: fixed; top: 0; left: 0; pointer-events: none;';
+    document.body.appendChild(myCanvas);
+  }
+
 }
