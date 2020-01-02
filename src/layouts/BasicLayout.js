377c0004ef7bb940edb8a9c57c2c0697c7e599ab
seven <756465934@qq.com>
2020-01-02 19:45:05 +0800

    修改iframe页面的title显示

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -202,6 +202,9 @@ class BasicLayout extends React.PureComponent {
     if (pathname.indexOf('examReadOnly') > 0) {
       title = '报考信息系统 - 小德'
     }
+    if (pathname.indexOf('/allReport/freeStudy')) {
+      title = '运营数据 - 小德'
+    }
 
     return title;
   }
