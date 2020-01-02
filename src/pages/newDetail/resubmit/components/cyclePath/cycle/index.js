4d474d1192b8299c5ddf0fb1c567c5f968a3fd17
wenwanyu <wenwanyu@sunlands.com>
2020-01-02 15:17:58 +0800

    修改文字

--- src/pages/newDetail/resubmit/components/cyclePath/cycle/index.js
+++ src/pages/newDetail/resubmit/components/cyclePath/cycle/index.js
@@ -34,7 +34,7 @@ class Cycle extends React.Component {
       <div className={styles.collegeWrap}>
         <p className={styles.title}>
           <span></span>
-          续费学员生命周期分布
+          续报学员生命周期分布
         </p>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Echarts
