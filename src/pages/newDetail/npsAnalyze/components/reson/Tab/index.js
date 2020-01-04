75709d3e4cca71f900eb6e76320ea8568b0931c5
wenwanyu <wenwanyu@sunlands.com>
2020-01-04 19:50:22 +0800

    原因分类

---
+++ src/pages/newDetail/npsAnalyze/components/reson/Tab/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Tabs } from 'antd';
+import './style.less';
+
+const TabPane = Tabs.TabPane;
+/*
+* Tabs 组件
+*
+* 基于原 ant Tabs
+* 只扩展自定义样式
+*
+* 不支持复用
+* */
+
+class BITabs extends React.Component {
+
+  render() {
+    return (
+      <span className='npsTabs'>
+        <Tabs {...this.props}>
+          {this.props.children}
+        </Tabs>
+      </span>
+    );
+  }
+}
+
+export default BITabs;
+BITabs.TabPane = TabPane;
--- src/pages/newDetail/npsAnalyze/components/reson/index.js
+++ src/pages/newDetail/npsAnalyze/components/reson/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from './style.less';
 import { connect } from 'dva';
+import BITabs from './Tab';
+
+const TabPane = BITabs.TabPane;
 
 // @connect(({ resubmitModal }) => ({
 //   resubmitModal,
@@ -8,22 +11,32 @@ import { connect } from 'dva';
 //   getCycleListData: resubmitModal.getCycleListData,
 // }))
 class Reson extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab: '1',
+    };
+  }
+  onTabChange = tab => {
+    this.setState({ tab });
+  };
   render() {
+    const { tab } = this.state;
     return (
       <div className={styles.collegeWrap}>
         <p className={styles.title}>
           <span></span>
-          续报学员生命周期分布
+          原因分类
         </p>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '240px',
-          }}
-        >
-          11
+        <div className={styles.resonWrap}>
+          <BITabs onChange={this.onTabChange} activeKey={'1'}>
+            <TabPane tab={tab} key={1}>
+              <div keye={1}>{1111}</div>
+            </TabPane>
+            <TabPane tab={tab} key={2}>
+              <div keye={1}>{22222222}</div>
+            </TabPane>
+          </BITabs>
         </div>
       </div>
     );
